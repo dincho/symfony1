6aee08a77e8413036467a3d2097421906d4d622c
dincho <dincho@xbuv.com>
2008-11-26 19:34:23 +0000

    Fixed actions for messages page when there is only one message in the box ( closes #141 )

--- web/js/messages.js
+++ web/js/messages.js
@@ -1,16 +1,26 @@
 function msg_select(chk, is_checked, read_unread)
 {
-    for (i = 0; i < chk.length; i++)
-    {
-        if( typeof(read_unread) != 'undefined' )
-        {
-            chk[i].checked = false; //clear all fields first
-            if( chk[i].getAttribute('read') == read_unread ) 
-            {
-                chk[i].checked = is_checked;
-            }
-        } else {
-            chk[i].checked = is_checked;
-        }
+    if( chk.length != undefined )
+    {    
+	    for (i = 0; i < chk.length; i++)
+	    {
+	        msg_select_item(chk[i], is_checked, read_unread);
+	    }
+    } else {
+        msg_select_item(chk, is_checked, read_unread);
     }
+}
+
+function msg_select_item(oItem, is_checked, read_unread)
+{
+	if( read_unread != undefined )
+	{
+	    oItem.checked = false; //clear all fields first
+	    if( oItem.getAttribute('read') == read_unread ) 
+	    {
+	        oItem.checked = is_checked;
+	    }
+	} else {
+	    oItem.checked = is_checked;
+	}
 }
