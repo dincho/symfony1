8ac9bcaa4355a5d02cf93b6a954ffc474aa5c8ca
dincho <dincho@xbuv.com>
2008-11-28 10:56:13 +0000

    Added delete functionality for Desc. Questions

--- apps/backend/modules/descAnswers/templates/listSuccess.php
+++ apps/backend/modules/descAnswers/templates/listSuccess.php
@@ -1,11 +1,12 @@
 <table class="zebra">
 <thead>
   <tr class="top_actions">
-    <td colspan="2"><?php echo button_to ('Add Answer', 'descAnswers/create?question_id=' . $question->getId()) ?></td>
+    <td colspan="3"><?php echo button_to ('Add Answer', 'descAnswers/create?question_id=' . $question->getId()) ?></td>
   </tr>
   <tr>
     <th>Title</th>
     <th>Search Criteria Title</th>
+    <th></th>
   </tr>
 </thead>
 <tbody>
@@ -13,6 +14,7 @@
   <tr rel="<?php echo url_for('descAnswers/edit?id=' . $answer->getId()) ?>" >
     <td><?php echo $answer->getTitle(); ?></td>
     <td><?php echo $answer->getSearchTitle(); ?></td>
+    <td class="delete"><?php echo link_to('Delete', 'descAnswers/delete?id=' . $answer->getId(), 'confirm=Are you sure you want to delete this answer?') ?></td>
   </tr>
   <?php endforeach; ?>
 </tbody>
