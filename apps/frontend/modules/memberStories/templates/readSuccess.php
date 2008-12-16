134a4d62f48b41a75668dd73a70016700795b996
dincho <dincho@xbuv.com>
2008-12-16 12:08:16 +0000

    added check if image of member story exists before showing. ( closes #212 )

--- apps/frontend/modules/memberStories/templates/readSuccess.php
+++ apps/frontend/modules/memberStories/templates/readSuccess.php
@@ -5,7 +5,10 @@
         </ul>
         <?php echo link_to(__('See all stories'), '@member_stories', 'class=sec_link') ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo link_to(__('Post your story'), 'memberStories/postYourStory', 'class=sec_link') ?>
     </div>
-    <?php echo image_tag('static/stories/story' . $story->getId() . '.jpg', array('id=member_story_img')) ?>
+    <?php $story_img = 'static/stories/story' . $story->getId() . '.jpg'; ?>
+    <?php if( file_exists( sfConfig::get('sf_web_dir') . '/images/' . $story_img ) ): ?>
+        <?php echo image_tag($story_img, array('id=member_story_img')) ?>
+    <?php endif; ?>
 </div>
 <div id="member_story_content">
     <?php echo $sf_data->getRaw('story')->getContent(); ?>
