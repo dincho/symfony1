7c11e5b901c7b276ac52cdff302d5440c28467ce
dincho <dincho@xbuv.com>
2008-12-21 14:16:55 +0000

    fixed "back" link in Report Bug page, now directs to previous page ( closes #128 )

--- apps/frontend/modules/content/templates/reportBugSuccess.php
+++ apps/frontend/modules/content/templates/reportBugSuccess.php
@@ -1,4 +1,4 @@
-<?php use_helper('dtForm') ?>
+<?php use_helper('dtForm', 'Javascript') ?>
 
 <?php echo __('Please describe what\'s not working or how we could improve our service:') ?>
 <?php echo form_tag('content/reportBug', array('id' => 'report_bug')) ?>
@@ -8,6 +8,6 @@
         <?php echo pr_label_for('description', 'Description:') ?>
         <?php echo textarea_tag('description', null, array('id' =>'description', 'class' => 'text_area', 'rows' => 15, 'cols' => 60)) ?>
     </fieldset>
-    <?php echo link_to(__('Cancel and go back to previous page'), 'dashboard/index', array('class' => 'sec_link_small')) ?><br />
+    <?php echo link_to_function(__('Cancel and go back to previous page'), 'window.history.go(-1)', array('class' => 'sec_link_small')) ?><br />
     <?php echo submit_tag(__('Send'), array('class' => 'button_mini')) ?>
 </form>
