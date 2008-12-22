27369df243ec72a9b654d3cfaa821828e91c2bf0
dincho <dincho@xbuv.com>
2008-12-22 10:50:01 +0000

    [backend] added IMBRA status to headline of member IMBRA ( closes #254 )

--- apps/backend/modules/members/templates/editIMBRASuccess.php
+++ apps/backend/modules/members/templates/editIMBRASuccess.php
@@ -1,6 +1,6 @@
 <?php echo button_to('Send Email', 'feedback/compose?mail_to=' . $member->getEmail(), 'class=float-right') ?>
 <br /><br />
 
-<div class="legend">IMBRA Application</div>
+<div class="legend"><?php echo $imbra->getImbraStatus() ?> IMBRA Application</div>
 <?php include_partial('imbra/edit', array('member' => $member, 'imbra' => $imbra)); ?>
 <?php include_partial('members/bottomMenu', array('member_id' => $member->getId())); ?>
