988250bc5d9d87d9fc36a3b794e9cb57557ca620
orlin <orlin@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-01-16 16:44:43 +0000

    Fixed Visual Bug ( #166 )

--- apps/frontend/modules/block/templates/indexSuccess.php
+++ apps/frontend/modules/block/templates/indexSuccess.php
@@ -1,13 +1,13 @@
-<?php echo __('To get new match results, change your %LINK_TO_SEARCH_CRITERIA%. <span>(To unblock a member click "Unblock")</span>',  array('%LINK_TO_SEARCH_CRITERIA%' => link_to(__('Search Criteria'), 'dashboard/searchCriteria'))) ?>
+<?php echo __('To get new match results, change your %LINK_TO_SEARCH_CRITERIA%. <span style="font-size: 11px">(To unblock a member click "Unblock")</span>',  array('%LINK_TO_SEARCH_CRITERIA%' => link_to(__('Search Criteria'), 'dashboard/searchCriteria', array('class' => 'sec_link')))) ?>
 <br /><br />
 <table class="blocked_members">
 <?php foreach ($blocks as $block): ?>
     <tr>
-		<td><?php echo $block->getMemberRelatedByProfileId()->getUsername() ?></td>
-		<td class="color-gray"><?php echo Tools::truncate($block->getMemberRelatedByProfileId()->getEssayHeadline(), 40) ?></td>
-		<td class="color-gray">ID: <?php echo $block->getProfileId() ?></td>
-		<td><?php echo link_to(__('View Profile'), '@profile?username=' . $block->getMemberRelatedByProfileId()->getUsername(), 'class=sec_link') ?></td>
-		<td><?php echo link_to(__('Unblock'), 'block/remove?id=' . $block->getId(), 'class=sec_link') ?></td>
-	</tr>
+        <td><?php echo $block->getMemberRelatedByProfileId()->getUsername() ?></td>
+        <td><span><?php echo Tools::truncate($block->getMemberRelatedByProfileId()->getEssayHeadline(), 40) ?></span></td>
+        <td><span>ID: <?php echo $block->getProfileId() ?></span></td>
+        <td><?php echo link_to(__('View Profile'), '@profile?username=' . $block->getMemberRelatedByProfileId()->getUsername(), 'class=sec_link') ?></td>
+        <td><?php echo link_to(__('Unblock'), 'block/remove?id=' . $block->getId(), 'class=sec_link') ?></td>
+    </tr>
 <?php endforeach; ?>
 </table>
