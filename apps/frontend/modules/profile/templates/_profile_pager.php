ca9e37c3f3fcee0f90578d4d88a44722688a06fb
dincho <dincho@xbuv.com>
2009-02-01 10:56:38 +0000

    added missing parameter to pager links ( closes #353 )

--- apps/frontend/modules/profile/templates/_profile_pager.php
+++ apps/frontend/modules/profile/templates/_profile_pager.php
@@ -1,13 +1,13 @@
 <div id="profile_pager">
     <?php if($sf_request->hasParameter('pager') && $sf_user->getAttribute('last_search_url') && $pager->hasResults()): ?>
         <div class="prev">
-            <?php echo link_to_unless(is_null($pager->getPrevious()), image_tag('prev.gif'), '@profile?username=' . $pager->getPrevious(), 'class=float-left') ?>
-            <?php echo link_to_unless(is_null($pager->getPrevious()), 'Previous', '@profile?username=' . $pager->getPrevious(), array('class' => 'float-left', 'style' => 'padding: 0 20px;')) ?>
+            <?php echo link_to_unless(is_null($pager->getPrevious()), image_tag('prev.gif'), '@profile?pager=1&username=' . $pager->getPrevious(), 'class=float-left') ?>
+            <?php echo link_to_unless(is_null($pager->getPrevious()), 'Previous', '@profile?pager=1&username=' . $pager->getPrevious(), array('class' => 'float-left', 'style' => 'padding: 0 20px;')) ?>
         </div>
         
         <div class="next">
-            <?php echo link_to_unless(is_null($pager->getNext()), image_tag('next.gif'), '@profile?username=' . $pager->getNext(), 'class=float-right no-padding') ?>
-            <?php echo link_to_unless(is_null($pager->getNext()), 'Next', '@profile?username=' . $pager->getNext(), array('class' => 'float-right', 'style' => 'padding: 0 20px;')) ?>
+            <?php echo link_to_unless(is_null($pager->getNext()), image_tag('next.gif'), '@profile?pager=1&username=' . $pager->getNext(), 'class=float-right no-padding') ?>
+            <?php echo link_to_unless(is_null($pager->getNext()), 'Next', '@profile?pager=1&username=' . $pager->getNext(), array('class' => 'float-right', 'style' => 'padding: 0 20px;')) ?>
         </div>
         
         <?php echo link_to(__('Return to Results'), $sf_user->getAttribute('last_search_url'), array('class' => 'sec_link')) ?>
