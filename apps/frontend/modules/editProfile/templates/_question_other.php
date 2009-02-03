d8c7aab6371fe97e173f36d529f96c9f3c560816
dincho <dincho@xbuv.com>
2009-02-03 18:54:43 +0000

    added radio class to radio button on self description pages ( #384)

--- apps/frontend/modules/editProfile/templates/_question_other.php
+++ apps/frontend/modules/editProfile/templates/_question_other.php
@@ -1,5 +1,5 @@
 <?php if( !is_null($question->getOther()) ): ?>
-    <?php echo radiobutton_tag('answers['. $question->getid() .']', 'other', fillIn( 'answers['. $question->getid() .']', 'r', false, isset($member_answers[$question->getId()]) && !is_null($member_answers[$question->getId()]->getOther()) ) ) ?>
+    <?php echo radiobutton_tag('answers['. $question->getid() .']', 'other', fillIn( 'answers['. $question->getid() .']', 'r', false, isset($member_answers[$question->getId()]) && !is_null($member_answers[$question->getId()]->getOther()) ), array('class' => 'radio') ) ?>
     <?php echo pr_label_for('answers['. $question->getid() .'][other]', $question->getOther()) ?><br />
     <?php echo input_tag('others['. $question->getid() .']', ( isset($member_answers[$question->getId()]) ) ? $member_answers[$question->getId()]->getOther() : null ) ?>
 <?php endif; ?>
