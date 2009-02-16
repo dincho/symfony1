79d93c7db70f02ce4ef33dd79d25e268a9672c99
orlin <orlin@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-02-16 19:04:54 +0000

    Fixed Visual Bug ( #560 )

--- apps/frontend/modules/search/templates/_filters.php
+++ apps/frontend/modules/search/templates/_filters.php
@@ -1,19 +1,22 @@
     <p class="search_p">
+    
         <?php echo checkbox_tag('filters[only_with_video]', 1, isset($filters['only_with_video'])) ?>
         <label for="filters_only_with_video"><?php echo __('Show only profiles with video') ?></label>
     </p>
     <p class="search_p" style="height: 35px; border: none;">
+    
         <?php echo __('Location') ?>
+        &nbsp;&nbsp;
         <?php echo radiobutton_tag('filters[location]', 0, ($filters['location'] == 0) ) ?>
         <label for="filter_location_0"><?php echo __('Everywhere') ?></label>
-        
+        &nbsp;&nbsp;&nbsp;
         <?php echo radiobutton_tag('filters[location]', 1, ($filters['location'] == 1) ) ?>
         <label for="filter_location_1"><?php echo __('In selected countries only') ?></label>
         <span><?php echo link_to(__('Select Countries'), 'search/selectCountries') ?></span>
-        
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <?php echo radiobutton_tag('filters[location]', 2, ($filters['location'] == 2) ) ?>
         <label for="filter_location_2"><?php echo __('In my area only') ?></label>
-        
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <?php echo checkbox_tag('filters[include_poland]', 1, isset($filters['include_poland'])  ) ?>
         <label for="filters_include_poland"><?php echo __('Include matches in Poland') ?></label>
         <span><?php echo link_to(__('Select Cities'), 'search/selectAreas?country=PL&polish_cities=1') ?></span>
