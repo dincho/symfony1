3b5fc362eb855653c0348e79a6623987263d280d
dincho <dincho@xbuv.com>
2009-02-17 12:15:18 +0000

    removed meta title tag from webResponse ( found an article that it is bad duplicate )

---
+++ apps/frontend/lib/prWebResponse.class.php
@@ -0,0 +1,12 @@
+<?php
+class prWebResponse extends sfWebResponse
+{
+	public function getMetas()
+	{
+		$meta_tags = $this->getParameterHolder ()->getAll ( 'helper/asset/auto/meta' );
+		if ( array_key_exists ('title', $meta_tags) ) {
+			unset($meta_tags ['title']);
+		}
+		return $meta_tags;
+	}
+}
