c84a4353d98d3b6c2af4372ca8e52132c74e0053
orlin <orlin@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-02-17 10:43:11 +0000

    Fixed Visual Bug ( #559 )

--- apps/frontend/modules/profile/templates/signInSuccess.php
+++ apps/frontend/modules/profile/templates/signInSuccess.php
@@ -5,7 +5,7 @@
 <div id="secondary_container">
 <?php echo form_tag('profile/signIn', array('id' => 'sign_in')) ?>
     <fieldset>
-        <legend><?php echo __('Sign in to continue...') ?></legend>
+        <div style="background-color: #000000; padding: 1px; padding-left: 8px; text-align: left;"><?php echo __('Sign in to continue...') ?></div>
         <?php if( isset($test_member) ): ?>
             <label for="email"><?php echo __('Email') ?></label> <input type="text" name="email" class="input_text_width" value="<?php echo $test_member->getEmail() ?>" style="width: 148px;" /><br />
             <label for="password"><?php echo __('Password') ?></label> <input type="password" name="password" class="input_text_width" size="20" value="<?php echo $test_member->getUsername() ?>" style="width: 148px;" /><br />
