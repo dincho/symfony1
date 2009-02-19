cf67f5737c0469cdaa654f6d16de6c798fe61c28
orlin <orlin@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-02-19 22:10:17 +0000

    Fixed Visual Bug ( #564 )

--- apps/frontend/modules/content/templates/_headerMenu.php
+++ apps/frontend/modules/content/templates/_headerMenu.php
@@ -8,11 +8,11 @@
             <?php echo link_to(image_tag('sign_in.gif'), 'profile/signIn') ?>
         </p>
     <?php else: ?>
-        <p>
+        <p style="margin-bottom: 2px;">
             <span class="username"><?php echo __('Hi %username%', array('%username%' => $username)) ?></span>
             <?php echo pr_link_to('Dashboard', 'dashboard/index') ?>&bull;<?php echo pr_link_to('Search', 'search/index') ?>&bull;<?php echo pr_link_to('Messages', 'messages/index', 'class=last') ?>
             <?php echo link_to(image_tag('sign_out.gif'), 'profile/signout') ?>
         </p>
-        <p class="second_row"><?php echo pr_link_to('Member Stories', 'memberStories/index') ?>&bull;<?php echo pr_link_to('Report a bug', 'content/reportBug') ?>&bull;<?php if(array_key_exists('help', $links_map)) echo link_to_unless( $sf_context->getModuleName() == 'content' && $sf_request->getParameter('slug') == 'help', $links_map['help'], '@page?slug=help', 'class=last') ?></p>
+        <p class="second_row" style="margin-top: 4px;"><?php echo pr_link_to('Member Stories', 'memberStories/index') ?>&bull;<?php echo pr_link_to('Report a bug', 'content/reportBug') ?>&bull;<?php if(array_key_exists('help', $links_map)) echo link_to_unless( $sf_context->getModuleName() == 'content' && $sf_request->getParameter('slug') == 'help', $links_map['help'], '@page?slug=help', 'class=last') ?></p>
     <?php endif; ?>
 </div>
