5bf4774e869248df69b7e6fd265241ca222f1c41
pavel <pavel@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-02-19 15:56:29 +0000

    fix #588

--- apps/backend/modules/staticPages/actions/components.class.php
+++ apps/backend/modules/staticPages/actions/components.class.php
@@ -11,12 +11,33 @@ class staticPagesComponents extends sfComponents
 {
      public function executeBestVideos()
      {
-         $c = new Criteria();
+         /*$c = new Criteria();
          $c->addJoin(MemberPeer::MEMBER_COUNTER_ID, MemberCounterPeer::ID);
          $c->addDescendingOrderByColumn(MemberCounterPeer::PROFILE_VIEWS);
          $c->add(MemberPeer::YOUTUBE_VID, null, Criteria::ISNOTNULL);
          $c->setLimit(3);
          
+         $this->members = MemberPeer::doSelect($c);*/
+         
+         $lastm = new DateTime($this->get_last_month());
+         $lastmonth = $lastm->format("Y-m");
+         $lastmonth = $lastmonth."%";
+         
+         $c = new Criteria();
+         $c->addJoin(MemberPeer::ID, ProfileViewPeer::PROFILE_ID);
+         $c->add(MemberPeer::YOUTUBE_VID, null, Criteria::ISNOTNULL);
+         $criteria = $c->getNewCriterion(ProfileViewPeer::CREATED_AT, $lastmonth, Criteria::LIKE);
+         $c->addAnd($criteria);
+         $c->addGroupByColumn(ProfileViewPeer::PROFILE_ID);
+         $c->setLimit(3);
+         
          $this->members = MemberPeer::doSelect($c);
+     }
+     
+     public static function get_last_month() 
+     {
+        return (date('m') == 1)?
+        join('-', array(date('Y')-1, '12')) :
+        join('-', array(date('Y'), date('m')-1)) ;
      }
 }
