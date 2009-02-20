dbe1a7c767ef7801d00c8b6cc18e2019b14e3097
orlin <orlin@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-02-20 17:27:54 +0000

    Fixed Visual Bug ( #572 )

--- apps/frontend/modules/content/templates/_homepagePhotoSet.php
+++ apps/frontend/modules/content/templates/_homepagePhotoSet.php
@@ -3,6 +3,8 @@
         <?php echo link_to(image_tag($photo->getTiltImageUrlPath()), 'registration/joinNow') ?>
     <?php else: ?>
         <?php $looking_for = ( $photo->getGender() == 'M') ? 'F_M' : 'M_F'; ?>
-        <?php echo link_to(image_tag($photo->getTiltImageUrlPath(), array('size' => '100x95')), 'search/public?filter=filter&filters[looking_for]=' . $looking_for) ?>
+        
+        <?php echo link_to(image_tag($photo->getTiltImageUrlPath(), array('size' => '93x91', 'class'=>'highlighted_image' )), 'search/public?filter=filter&filters[looking_for]=' . $looking_for, array( 'class' => 'highlighted_link')) ?>
+        
     <?php endif; ?>
 <?php endforeach; ?>
