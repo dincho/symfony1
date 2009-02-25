2a4d03c673cd46e88b8dd0c104589bc38ff7cbbd
todor <todor@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-02-25 16:33:22 +0000

    #609

--- apps/frontend/modules/content/templates/pageSuccess.php
+++ apps/frontend/modules/content/templates/pageSuccess.php
@@ -2,3 +2,6 @@
 <?php slot('header_title') ?>
     <?php echo $page->getTitle() ?>
 <?php end_slot(); ?>
+<?php slot('footer_menu') ?>
+    <?php include_partial('content/footer_menu') ?>
+<?php end_slot(); ?>
