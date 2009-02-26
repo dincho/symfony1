26c183e16cb6d06880e73e04f3696d5a09f164e5
orlin <orlin@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-02-26 17:01:30 +0000

    Fixed Visual Bug ( No Ticket ) Admin Panel Login Page

--- apps/backend/modules/system/templates/loginSuccess.php
+++ apps/backend/modules/system/templates/loginSuccess.php
@@ -1,11 +1,13 @@
-<?php echo form_tag('system/login', 'class=form style=width: 550px; margin: auto; display: block;') ?>
+<?php echo form_tag('system/login', 'class=form2 style=width: 550px; margin: auto; display: block;') ?>
   <div class="legend">Sign In</div>
   <fieldset class="form_fields" style="margin: 15px 0">
+    <div style="width: 280px; height: 60px; margin-left: 120px;">
     <label for="username">Username:</label>
     <?php echo input_tag('username') ?><br />
-    
+    <br />
     <label for="password">Password:</label>
     <?php echo input_password_tag('password') ?><br />
+    </div>
   </fieldset>
   <fieldset class="actions">
     <?php echo submit_tag('Login') ?>
