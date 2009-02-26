506163770e8750297b6d74d53065b4ea2f2aa8e0
todor <todor@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-02-26 12:48:35 +0000

    #609 added changes

--- apps/frontend/modules/content/templates/pageSuccess.php
+++ apps/frontend/modules/content/templates/pageSuccess.php
@@ -2,6 +2,10 @@
 <?php slot('header_title') ?>
     <?php echo $page->getTitle() ?>
 <?php end_slot(); ?>
-<?php slot('footer_menu') ?>
-    <?php include_partial('content/footer_menu') ?>
-<?php end_slot(); ?>
+<?php
+if ($sf_request->getParameter('slug') === 'affiliates') {
+	slot('footer_menu');
+		include_partial('content/footer_menu');
+	end_slot(); 
+} 
+?>
