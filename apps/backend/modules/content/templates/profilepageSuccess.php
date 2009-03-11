f8e3ec7531c7e9ccd7dd76e4bae9d5d8ef689f5b
orlin <orlin@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-03-11 18:34:34 +0000

    Fixed Visual Bug ( #382 )

--- apps/backend/modules/content/templates/profilepageSuccess.php
+++ apps/backend/modules/content/templates/profilepageSuccess.php
@@ -22,8 +22,8 @@
         <?php echo textarea_tag('trans[9]', (isset($trans[9])) ? $trans[9]->getTarget() : null, array('cols' => 60, 'rows' => 9)) ?><br />
     </fieldset>
     
-    <fieldset class="form_fields error_msgs_fields">
-        <label>Error Messages</label>
+    <fieldset class="form_fields error_msgs_fields" id="labels_120">
+        <label style="width: 120px;">Error Messages</label>
         <?php echo input_tag('trans[10]', (isset($trans[10])) ? $trans[10]->getTarget() : null) ?><br />
         <label></label><?php echo input_tag('trans[79]', (isset($trans[79])) ? $trans[79]->getTarget() : null) ?><br />
         <label></label><?php echo input_tag('trans[80]', (isset($trans[80])) ? $trans[80]->getTarget() : null) ?><br />
@@ -32,36 +32,36 @@
         <label></label><?php echo input_tag('trans[119]', (isset($trans[119])) ? $trans[119]->getTarget() : null) ?><br />
         <label></label><?php echo input_tag('trans[135]', (isset($trans[135])) ? $trans[135]->getTarget() : null) ?><br />
 
-        <br /><label>To Free members</label>
-        <label>upgrade to use</label><?php echo input_tag('trans[137]', (isset($trans[137])) ? $trans[137]->getTarget() : null) ?><br />
-        <label>upgrade to send</label><?php echo input_tag('trans[86]', (isset($trans[86])) ? $trans[86]->getTarget() : null) ?><br />
+        <br /><label style="width: 120px;">To Free members</label><br />
+        <label>upg. to use</label><?php echo input_tag('trans[137]', (isset($trans[137])) ? $trans[137]->getTarget() : null) ?><br />
+        <label>upg. to send</label><?php echo input_tag('trans[86]', (isset($trans[86])) ? $trans[86]->getTarget() : null) ?><br />
         <label>send limit</label><?php echo input_tag('trans[124]', (isset($trans[124])) ? $trans[124]->getTarget() : null) ?><br />
-        <label>upgrade to read</label><?php echo input_tag('trans[125]', (isset($trans[125])) ? $trans[125]->getTarget() : null) ?><br />        
+        <label>upg. to read</label><?php echo input_tag('trans[125]', (isset($trans[125])) ? $trans[125]->getTarget() : null) ?><br />        
         <label>read limit</label><?php echo input_tag('trans[77]', (isset($trans[77])) ? $trans[77]->getTarget() : null) ?><br />
-        <label>upgrade to reply</label><?php echo input_tag('trans[78]', (isset($trans[78])) ? $trans[78]->getTarget() : null) ?><br />
+        <label>upg. to reply</label><?php echo input_tag('trans[78]', (isset($trans[78])) ? $trans[78]->getTarget() : null) ?><br />
         <label>reply limit</label><?php echo input_tag('trans[81]', (isset($trans[81])) ? $trans[81]->getTarget() : null) ?><br />
-        <label>upgrade to post upload</label><?php echo input_tag('trans[82]', (isset($trans[82])) ? $trans[82]->getTarget() : null) ?><br />        
+        <label>upg. to post upl.</label><?php echo input_tag('trans[82]', (isset($trans[82])) ? $trans[82]->getTarget() : null) ?><br />        
         <label>post photo limit</label><?php echo input_tag('trans[115]', (isset($trans[115])) ? $trans[115]->getTarget() : null) ?><br />
-        <label>upgrade to see viewers</label><?php echo input_tag('trans[116]', (isset($trans[116])) ? $trans[116]->getTarget() : null) ?><br />
-        <label>upgrade to contact assistant</label><?php echo input_tag('trans[84]', (isset($trans[84])) ? $trans[84]->getTarget() : null) ?><br />
-        <label>contact assistant limit</label><?php echo input_tag('trans[87]', (isset($trans[87])) ? $trans[87]->getTarget() : null) ?><br />
-        <label>upgrade to wink</label><?php echo input_tag('trans[88]', (isset($trans[88])) ? $trans[88]->getTarget() : null) ?><br />
+        <label>upg. to see view.</label><?php echo input_tag('trans[116]', (isset($trans[116])) ? $trans[116]->getTarget() : null) ?><br />
+        <label>upg. to cont. assist.</label><?php echo input_tag('trans[84]', (isset($trans[84])) ? $trans[84]->getTarget() : null) ?><br />
+        <label>cont. assist. limit</label><?php echo input_tag('trans[87]', (isset($trans[87])) ? $trans[87]->getTarget() : null) ?><br />
+        <label>upg. to wink</label><?php echo input_tag('trans[88]', (isset($trans[88])) ? $trans[88]->getTarget() : null) ?><br />
         <label>wink limit</label><?php echo input_tag('trans[117]', (isset($trans[117])) ? $trans[117]->getTarget() : null) ?><br />
         <label></label><?php echo input_tag('trans[118]', (isset($trans[118])) ? $trans[118]->getTarget() : null) ?><br />
         
-        <br /><label>To Paid members</label>
-        <label>upgrade to send</label><?php echo input_tag('trans[126]', (isset($trans[126])) ? $trans[126]->getTarget() : null) ?><br />
+        <br /><label style="width: 120px;">To Paid members</label><br />
+        <label>upg. to send</label><?php echo input_tag('trans[126]', (isset($trans[126])) ? $trans[126]->getTarget() : null) ?><br />
         <label>send limit</label><?php echo input_tag('trans[127]', (isset($trans[127])) ? $trans[127]->getTarget() : null) ?><br />        
-        <label>upgrade to read</label><?php echo input_tag('trans[120]', (isset($trans[120])) ? $trans[120]->getTarget() : null) ?><br />
+        <label>upg. to read</label><?php echo input_tag('trans[120]', (isset($trans[120])) ? $trans[120]->getTarget() : null) ?><br />
         <label>read limit</label><?php echo input_tag('trans[121]', (isset($trans[121])) ? $trans[121]->getTarget() : null) ?><br />
-        <label>upgrade to reply</label><?php echo input_tag('trans[122]', (isset($trans[122])) ? $trans[122]->getTarget() : null) ?><br />
+        <label>upg. to reply</label><?php echo input_tag('trans[122]', (isset($trans[122])) ? $trans[122]->getTarget() : null) ?><br />
         <label>reply limit</label><?php echo input_tag('trans[123]', (isset($trans[123])) ? $trans[123]->getTarget() : null) ?><br />
-        <label>upgrade to post upload</label><?php echo input_tag('trans[128]', (isset($trans[128])) ? $trans[128]->getTarget() : null) ?><br />
+        <label>upg. to post upl.</label><?php echo input_tag('trans[128]', (isset($trans[128])) ? $trans[128]->getTarget() : null) ?><br />
         <label>post photo limit</label><?php echo input_tag('trans[129]', (isset($trans[129])) ? $trans[129]->getTarget() : null) ?><br />
-        <label>upgrade to see viewers</label><?php echo input_tag('trans[130]', (isset($trans[130])) ? $trans[130]->getTarget() : null) ?><br />
-        <label>upgrade to contact assistant</label><?php echo input_tag('trans[131]', (isset($trans[131])) ? $trans[131]->getTarget() : null) ?><br />
-        <label>contact assistant limit</label><?php echo input_tag('trans[132]', (isset($trans[132])) ? $trans[132]->getTarget() : null) ?><br />
-        <label>upgrade to wink</label><?php echo input_tag('trans[133]', (isset($trans[133])) ? $trans[133]->getTarget() : null) ?><br />
+        <label>upg. to see view.</label><?php echo input_tag('trans[130]', (isset($trans[130])) ? $trans[130]->getTarget() : null) ?><br />
+        <label>upg. to cont. assist.</label><?php echo input_tag('trans[131]', (isset($trans[131])) ? $trans[131]->getTarget() : null) ?><br />
+        <label>cont. assist. limit</label><?php echo input_tag('trans[132]', (isset($trans[132])) ? $trans[132]->getTarget() : null) ?><br />
+        <label>upg. to wink</label><?php echo input_tag('trans[133]', (isset($trans[133])) ? $trans[133]->getTarget() : null) ?><br />
         <label>wink limit</label><?php echo input_tag('trans[134]', (isset($trans[134])) ? $trans[134]->getTarget() : null) ?><br />
     </fieldset>
     
