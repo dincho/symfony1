88f89235171f903e82d86584632fa84b79def546
dincho <dincho@xbuv.com>
2009-03-17 10:41:03 +0000

    added cancel link to manage subscriptions page ( closes #727 )

--- apps/frontend/modules/subscription/templates/manageSuccess.php
+++ apps/frontend/modules/subscription/templates/manageSuccess.php
@@ -8,7 +8,9 @@
         <?php echo __('If you unsubscribe now, you will still use Full Member account until the end of the subscription period - that is until %EOT_DATE%. On that day the system will automatically switch your membership to Basic account and you will still be able to use our website.<br />However Full Member\'s  features will not be available to you.<br />', array('%EOT_DATE%' => date('M d, Y', $member->getEotDate()))) ?>
         <br />
         <?php echo __('If you still want to unsubscribe, please click UNSUBSCRIBE button below.<br />If you changed your mind or want to unsubscribe later, please <a href="%URL_FOR_DASHBOARD%" class="sec_link_small">Cancel and go to dashboard.</a>', array('%URL_FOR_DASHBOARD%' => url_for('@dashboard'))) ?><br />
-        <br /><?php echo button_to(__('Unsubscribe'), sfConfig::get('app_paypal_url') . '?cmd=_subscr-find&alias=' . urlencode(sfConfig::get('app_paypal_business')), array('popup' => true, 'class' => 'button')) ?>    
+        <br /><br />
+        <?php echo link_to(__('Cancel and go to dashboard'), '@dashboard', array('class' => 'sec_link')) ?><br />
+        <?php echo button_to(__('Unsubscribe'), sfConfig::get('app_paypal_url') . '?cmd=_subscr-find&alias=' . urlencode(sfConfig::get('app_paypal_business')), array('popup' => true, 'class' => 'button')) ?>    
     <?php else: ?>
          <?php echo __('Your subscription is canceled. That means your account will not be automatically charged.') ?>
          <br /><br />
