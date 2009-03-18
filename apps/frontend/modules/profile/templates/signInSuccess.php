887c0d70256f7114f4165157fdf392e19d20e64a
dincho <dincho@xbuv.com>
2009-03-18 16:14:51 +0000

    removed some test conditions from login form

--- apps/frontend/modules/profile/templates/signInSuccess.php
+++ apps/frontend/modules/profile/templates/signInSuccess.php
@@ -6,13 +6,8 @@
 <?php echo form_tag('profile/signIn', array('id' => 'sign_in')) ?>
     <fieldset>
         <div style="background-color: #000000; padding: 1px; padding-left: 8px; text-align: left;"><?php echo __('Sign in to continue...') ?></div>
-        <?php if( isset($test_member) ): ?>
-            <label for="email"><?php echo __('Email') ?></label> <input type="text" name="email" class="input_text_width" value="<?php echo $test_member->getEmail() ?>" style="width: 148px;" /><br />
-            <label for="password"><?php echo __('Password') ?></label> <input type="password" name="password" class="input_text_width" size="20" value="<?php echo $test_member->getUsername() ?>" style="width: 148px;" /><br />
-        <?php else: ?>
-            <label for="email"><?php echo __('Email') ?></label> <input type="text" name="email" class="input_text_width" style="width: 148px;" /><br />
-            <label for="password"><?php echo __('Password') ?></label> <input type="password" name="password" class="input_text_width" size="20" style="width: 148px;" /><br />        
-        <?php endif; ?>
+        <label for="email"><?php echo __('Email') ?></label> <input type="text" name="email" class="input_text_width" style="width: 148px;" /><br />
+        <label for="password"><?php echo __('Password') ?></label> <input type="password" name="password" class="input_text_width" size="20" style="width: 148px;" /><br />        
         <span><?php echo link_to('Forgot your Password?', 'profile/forgotYourPassword', array('class' => 'sec_link_small')) ?></span>
     </fieldset>
     <br/>
