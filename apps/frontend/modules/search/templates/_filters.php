870bcb4fd3466519f91b3ad90962244c7e968d14
orlin <orlin@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-04-02 14:56:05 +0000

    Fixed Visual Bug ( #759 ) Class Names Correction

--- apps/frontend/modules/search/templates/_filters.php
+++ apps/frontend/modules/search/templates/_filters.php
@@ -1,16 +1,16 @@
     <table border="0" cellpadding="0" cellspacing="0" class="search_filter">
 	    <tbody>
-		    <tr class="tr_1">
+		    <tr class="search_filter_tr_1">
 		        <td colspan="5">
 		        <?php echo checkbox_tag('filters[only_with_video]', 1, isset($filters['only_with_video'])) ?>&nbsp;
 		        <?php echo __('Show only profiles with video') ?>
 		        </td>
 		    </tr>
 		    <tr>
-		        <td colspan="5" class="td_2"></td>
+		        <td colspan="5" class="search_filter_td_2"></td>
 		    </tr>
-		    <tr class="tr_1">
-		        <td class="td_3"><strong><?php echo __('Location') ?></strong></td>
+		    <tr class="search_filter_tr_1">
+		        <td class="search_filter_td_3"><strong><?php echo __('Location') ?></strong></td>
 		        <td> <?php echo radiobutton_tag('filters[location]', 0, ($filters['location'] == 0) ) ?>&nbsp;
 		                <?php echo __('Everywhere') ?>
 		        </td>
@@ -26,16 +26,16 @@
 		        <?php echo __('Include matches in Poland') ?>
 		        </td>
 		    </tr>
-		    <tr class="tr_1">
+		    <tr class="search_filter_tr_1">
 		        <td></td>
 		        <td></td>
 		
-		        <td class="td_4"><?php echo link_to(__('Select Countries'), 'search/selectCountries') ?></td>
+		        <td class="search_filter_td_4"><?php echo link_to(__('Select Countries'), 'search/selectCountries') ?></td>
 		        <td></td>
-		        <td class="td_4"><?php echo link_to(__('Select Cities'), 'search/selectAreas?country=PL&polish_cities=1') ?></td>
+		        <td class="search_filter_td_4"><?php echo link_to(__('Select Cities'), 'search/selectAreas?country=PL&polish_cities=1') ?></td>
 		    </tr>
 		    <tr>
-		        <td colspan="5" class="td_5" >
+		        <td colspan="5" class="search_filter_td_5" >
 		        <?php echo submit_tag(__('Search'), array('class' => 'button','name' => 'filter', 'onclick' => "show_loader('match_results')")) ?>
 		        </td>
 		
--- apps/frontend/modules/search/templates/publicSuccess.php
+++ apps/frontend/modules/search/templates/publicSuccess.php
@@ -4,8 +4,8 @@
 <form action="<?php echo url_for('search/' . sfContext::getInstance()->getActionName()) ?>" id="search_box" class="public_matches">
     <table border="0" cellpadding="0" cellspacing="0" class="search_filter">
 	    <tbody>
-		    <tr class="tr_1">
-		        <td class="td_1"><?php echo __('You are') ?></td>
+		    <tr class="search_filter_tr_1">
+		        <td class="search_filter_td_1"><?php echo __('You are') ?></td>
 		        <td colspan="2" style="width: 280px;">
 		        <?php echo select_tag('filters[looking_for]', looking_for_options(isset($filters['looking_for']) ? $filters['looking_for'] : null)) ?>
 		        </td>
@@ -20,26 +20,26 @@
 		        </td>
 		    </tr>
 		    <tr>
-		        <td colspan="5" class="td_2"></td>
+		        <td colspan="5" class="search_filter_td_2"></td>
 		    </tr>
-		    <tr class="tr_1">
-		        <td class="td_3"><strong><?php echo __('Location') ?></strong></td>
+		    <tr class="search_filter_tr_1">
+		        <td class="search_filter_td_3"><strong><?php echo __('Location') ?></strong></td>
 		        <td> <?php echo radiobutton_tag('filters[location]', 0, ($filters['location'] == 0), array('class' => 'margin_right_zero') ) ?>&nbsp;<?php echo __('Everywhere') ?></td>
 		
 		        <td><?php echo radiobutton_tag('filters[location]', 1, ($filters['location'] == 1), array('class' => 'margin_right_zero') ) ?>&nbsp;<?php echo __('In selected countries only') ?></td>
 		        <td><?php echo radiobutton_tag('filters[location]', 2, ($filters['location'] == 2), array('class' => 'margin_right_zero') ) ?>&nbsp;<?php echo __('In my area only') ?></td>
 		        <td><?php echo checkbox_tag('filters[include_poland]', 1, isset($filters['include_poland']), array('class' => 'margin_right_zero')  ) ?>&nbsp;<?php echo __('Include matches in Poland') ?></td>
 		    </tr>
-		    <tr class="tr_1">
+		    <tr class="search_filter_tr_1">
 		        <td></td>
 		        <td></td>
 		
-		        <td class="td_4"><?php echo link_to(__('Select Countries'), 'search/selectCountries') ?></td>
+		        <td class="search_filter_td_4"><?php echo link_to(__('Select Countries'), 'search/selectCountries') ?></td>
 		        <td></td>
-		        <td class="td_4"><?php echo link_to(__('Select Cities'), 'search/selectAreas?country=PL&polish_cities=1') ?></td>
+		        <td class="search_filter_td_4"><?php echo link_to(__('Select Cities'), 'search/selectAreas?country=PL&polish_cities=1') ?></td>
 		    </tr>
 		    <tr>
-		        <td colspan="5" class="td_5" ><?php echo submit_tag(__('Search'), array('class' => 'button','name' => 'filter', 'onclick' => "show_loader('match_results')")) ?></td>
+		        <td colspan="5" class="search_filter_td_5" ><?php echo submit_tag(__('Search'), array('class' => 'button','name' => 'filter', 'onclick' => "show_loader('match_results')")) ?></td>
 		
 		    </tr>
 	    </tbody>
