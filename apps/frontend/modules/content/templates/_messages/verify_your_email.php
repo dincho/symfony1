24ab8d555b62ccd36118181db868226f8c2bc380
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-04-13 16:41:25 +0000

    removed extra BC item for verify your email from the template

--- apps/frontend/modules/content/templates/_messages/verify_your_email.php
+++ apps/frontend/modules/content/templates/_messages/verify_your_email.php
@@ -2,5 +2,4 @@
 <?php echo 'Verify your email headline' ?>
 <?php end_slot(); ?>
 
-<?php $sf_user->getBC()->add(array('name' => 'Verify your email headline')); ?>
 <?php echo __('For your own security, please go to your email box and verify your email by clicking on the link we have just sent you.') ?>
