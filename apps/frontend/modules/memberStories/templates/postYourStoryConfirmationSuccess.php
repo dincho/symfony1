e5534199aca87db772adf0785757e5ca2c1f9b6c
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-04-13 11:51:40 +0000

    changed TU of post your story confirmation page ( refs #813 )

--- apps/frontend/modules/memberStories/templates/postYourStoryConfirmationSuccess.php
+++ apps/frontend/modules/memberStories/templates/postYourStoryConfirmationSuccess.php
@@ -1,6 +1,1 @@
-<?php echo __('Our team will review your story and hopefully it will be available online soon.') ?><br /><br />
-<?php echo __('Please note that according to our <a href="%URL_FOR_TERMS%" class="sec_link">Terms of Use</a> and 
-               <a href="%URL_FOR_PRIVACY_POLICY%" class="sec_link">Privacy Policy</a>, we reserve the right to cut, 
-               edit or otherwise change your story without any explanation. We may also take your story off our site at any time or not publish it at all. 
-               We also do not respond to any queries related to the stories, except for request to take the story off the site. 
-               In order to do so however, you must write to us from the same email address as you entered while submitting the story') ?>
+<?php echo __('Post your story confirmation body'); ?>
