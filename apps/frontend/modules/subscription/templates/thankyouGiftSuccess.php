d594b20d7699d6767387b88c333e00afb7ee4356
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-04-15 09:00:41 +0000

    added new TU "Thank you gift body" for thank you page of gift membership ( closes #842 )

--- apps/frontend/modules/subscription/templates/thankyouGiftSuccess.php
+++ apps/frontend/modules/subscription/templates/thankyouGiftSuccess.php
@@ -1,3 +1,2 @@
-It is always a pleasure to see members buying gift for others. Thank you. <br /><br />
-Please note that while the gift receiver should and hopefully will be grateful to you, she or he is, according to the law and our <a href="user_agreement_m.shtml" class="sec_link">Terms of Use</a>, is not required to reciprocate in any way and make still block or flag your profile etc. Just like in real life, buying a gift may be a great way of making a contact or strengthening the existing one, but it can not justify any inappropriate or illegal behavior.<br /><br />
+<?php echo __('Thank you gift body'); ?>
 <?php echo link_to(__('Back to profile'), '@profile?username=' . $member->getUsername(), array('class' => 'sec_link')) ?>
