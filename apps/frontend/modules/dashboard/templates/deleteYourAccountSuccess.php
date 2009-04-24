ec07bbd38e9ebcb06bad1ba1dfd122424b1791bd
pavel <pavel@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-04-24 12:12:42 +0000

    Merge TUs for Delete Account page for Ticket #875

--- apps/frontend/modules/dashboard/templates/deleteYourAccountSuccess.php
+++ apps/frontend/modules/dashboard/templates/deleteYourAccountSuccess.php
@@ -1,8 +1,5 @@
-<?php echo __('Here you can delete your account - you will lose your information and you won\'t be able to recover it.') ?><br />
-<span><?php echo __('Deleting your account cannot be reversed. You will loose all your information, including profile, photos and messages.<br />Are you sure you want to delete?') ?></span><br /><br />
-<?php echo link_to(__('Yes, delete my account anyway'), 'dashboard/deleteConfirmation', array('class' => 'sec_link')) ?><br /><br />
-<?php echo link_to(__('No, don\'t delete my account'), 'dashboard/index', array('class' => 'sec_link')) ?><br /><br />
-
+<?php echo __('Here you can delete your account - you will lose your information and you won\'t be able to recover it. <br /><span>Deleting your account cannot be reversed. You will loose all your information, including profile, photos and messages.<br />Are you sure you want to delete?</span><br /><br /><a href="%URL_FOR_YES_DELETE%" class="sec_link">Yes, delete my account anyway</a><br /><br/><a href="%URL_FOR_NO_DELETE%" class="sec_link">No, don\'t delete my account</a>', array('%URL_FOR_YES_DELETE%' => url_for('dashboard/deleteConfirmation'), '%URL_FOR_NO_DELETE%' => url_for('dashboard/index'))) ?><br /> <br />
+<br /> <br />
 <?php slot('footer_menu') ?>
     <?php include_partial('content/footer_menu') ?>
 <?php end_slot(); ?>
