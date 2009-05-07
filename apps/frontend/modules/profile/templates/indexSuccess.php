93da48894fb0b158959c14b700d61798fcfa4969
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-05-07 09:03:49 +0000

    implemented "dimmed" behavior at view profile page for "block" link ( closes #915 )

--- apps/frontend/modules/profile/templates/indexSuccess.php
+++ apps/frontend/modules/profile/templates/indexSuccess.php
@@ -7,7 +7,7 @@
         <?php echo link_to(__('Wink'), 'winks/send?profile_id=' . $member->getId(), 'class=sec_link') ?>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;
         <?php echo link_to(__('Send Mail'), 'messages/send?profile_id=' . $member->getId(), 'class=sec_link') ?>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;
         <?php echo link_to(__('Add to Hotlist'), 'hotlist/add?profile_id=' . $member->getId(), 'class=sec_link') ?>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;
-        <?php echo link_to(__('Block'), 'block/add?profile_id=' . $member->getId(), 'class=sec_link') ?>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;
+        <?php echo link_to_unless($sf_user->getProfile()->hasBlockFor($member->getId()), __('Block'), 'block/add?profile_id=' . $member->getId(), 'class=sec_link') ?>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;
         <?php echo link_to(__('Flag'), 'content/flag?username=' . $member->getUsername(), 'class=sec_link') ?>
    </div>
     <span class="profile_gift">
