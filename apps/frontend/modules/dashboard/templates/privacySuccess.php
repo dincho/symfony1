676d1c5e57665ac966bb873979c025286cb8f45b
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-05-08 15:15:35 +0000

    merged some TU's on privacy options page ( closes #922 )

--- apps/frontend/modules/dashboard/templates/privacySuccess.php
+++ apps/frontend/modules/dashboard/templates/privacySuccess.php
@@ -1,7 +1,7 @@
 <?php use_helper('Object', 'dtForm') ?>
 
-<?php echo __('Here you can change your Privacy Settings.') ?><br />
-<span><?php echo __('Change selection and click Save.') ?></span><br />
+<?php echo __('Privacy options headline content'); ?>
+
 <?php echo form_tag('dashboard/privacy', array('id' => 'privacy')) ?>
     <?php echo object_checkbox_tag($member, 'getDontUsePhotos') ?>
     <?php echo pr_label_for('dont_use_photos', __('I don\'t want my photos to be used on the home page.')) ?><br />
@@ -9,10 +9,8 @@
     <?php echo object_checkbox_tag($member, 'getContactOnlyFullMembers') ?>
     <?php echo pr_label_for('contact_only_full_members', __('I want to be contacted by Full Members only.')) ?><br />
     
-    <div class="center_text">
-        <?php echo __('You may also deactivate (hide) your profile by <a href="%URL_FOR_DEACTIVATE_PROFILE%" class="sec_link">clicking here</a>.') ?><br /><br />
-        <?php echo __('You may also delete your account by <a href="%URL_FOR_DELETE_ACCOUNT%" class="actions">clicking here</a>.') ?>
-    </div>
+		<?php echo __('Privacy options footer content'); ?>
+		
     <?php echo link_to(__('Cancel and go to dashboard'), 'dashboard/index', array('class' => 'sec_link_small')) ?><br />
     <?php echo submit_tag(__('Save'), array('class' => 'button')) ?>
 </form>
