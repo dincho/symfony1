d10adc348f9b13d681de90b1ee2e52f556624905
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-05-12 17:22:44 +0000

    Added some ignores and added symfony libs as svn:externals, version 1.0.20

--- config/config.php
+++ config/config.php
@@ -1,16 +1,7 @@
 <?php
-
-// symfony directories (Linux)
-//$sf_symfony_lib_dir  = '/usr/share/php/symfony';
-//$sf_symfony_data_dir = '/usr/share/php/data/symfony';
-
-//symfony directories (osx)
-$sf_symfony_lib_dir  = '/usr/share/php/symfony10/lib';
-$sf_symfony_data_dir = '/usr/share/php/symfony10/data';
-
-//symfony directories (Orlin's windows)
-//$sf_symfony_lib_dir  = 'C:\_symfony_installed_\symfony-1.0.19\symfony-1.0.19\lib';
-//$sf_symfony_data_dir = 'C:\_symfony_installed_\symfony-1.0.19\symfony-1.0.19\data';
+// symfony directories
+$sf_symfony_lib_dir  = dirname(__FILE__).'/../lib/symfony';
+$sf_symfony_data_dir = dirname(__FILE__).'/../data/symfony';
 
 if( !defined('SALT') ) define('SALT', 'fj`Jbu6`r7XFvC$V@<%3l#lE/K/J\;Ai');
 if( !defined('BASE_URL') ) define('BASE_URL', 'http://pd.dincho.bonex.us/');
