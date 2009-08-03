093c06ae8fea64a013214fa4ab2655b0b031a3b6
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-08-03 17:10:09 +0000

    marked "Forgot your Password?" on sign in page for i18n

--- apps/frontend/modules/profile/templates/signInSuccess.php
+++ apps/frontend/modules/profile/templates/signInSuccess.php
@@ -9,7 +9,7 @@
         <div style="background-color: #000000; padding: 1px; padding-left: 8px; text-align: left;"><?php echo __('Sign in to continue...') ?></div>
         <label for="email"><?php echo __('Email') ?></label> <input type="text" name="email" class="input_text_width" style="width: 148px;" /><br />
         <label for="password"><?php echo __('Password') ?></label> <input type="password" name="password" class="input_text_width" size="20" style="width: 148px;" /><br />        
-        <span><?php echo link_to('Forgot your Password?', 'profile/forgotYourPassword', array('class' => 'sec_link_small')) ?></span>
+        <span><?php echo link_to(__('Forgot your Password?'), 'profile/forgotYourPassword', array('class' => 'sec_link_small')) ?></span>
     </fieldset>
     <br/>
     <?php echo submit_tag('Sign In', array('class' => 'button sign_in')) ?><br /><br />
