d13c9611c6f919e1d718013f3ddb583a8d66d9a5
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-08-05 11:40:43 +0000

    re-writed a little bit the template of sign-in page to validate the xHTML ( fixes #1009 )

--- apps/frontend/modules/profile/templates/signInSuccess.php
+++ apps/frontend/modules/profile/templates/signInSuccess.php
@@ -2,17 +2,18 @@
     <?php echo __('Member Sign In') ?>
 <?php end_slot(); ?>
 
-<div id="secondary_container">
 <?php echo form_tag('profile/signIn', array('id' => 'sign_in')) ?>
     <?php echo input_hidden_tag('referer', $sf_request->getAttribute('referer')) ?>
     <fieldset>
         <div style="background-color: #000000; padding: 1px; padding-left: 8px; text-align: left;"><?php echo __('Sign in to continue...') ?></div>
-        <label for="email"><?php echo __('Email') ?></label> <input type="text" name="email" class="input_text_width" style="width: 148px;" /><br />
-        <label for="password"><?php echo __('Password') ?></label> <input type="password" name="password" class="input_text_width" size="20" style="width: 148px;" /><br />        
+        <label for="email"><?php echo __('Email') ?></label>
+        <?php echo input_tag('email', null, array('class' => 'input_text_width')); ?><br />
+        
+        <label for="password"><?php echo __('Password') ?></label>
+        <?php echo input_password_tag('password', null, array('class' => 'input_text_width')); ?><br />
         <span><?php echo link_to(__('Forgot your Password?'), 'profile/forgotYourPassword', array('class' => 'sec_link_small')) ?></span>
     </fieldset>
     <br/>
     <?php echo submit_tag('Sign In', array('class' => 'button sign_in')) ?><br /><br />
     <?php echo __('New to PolishRomance.com? <a href="%URL_FOR_JOIN_NOW%" class="sec_link">Join for free</a>') ?>
 </form>
-</div>
