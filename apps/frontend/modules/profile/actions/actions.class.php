433a1f35e2b81177c2bee5a179c08dc3486629df
pavel <pavel@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-08-07 12:37:49 +0000

    Remove the forgotten comment from the action class - refs #995

--- apps/frontend/modules/profile/actions/actions.class.php
+++ apps/frontend/modules/profile/actions/actions.class.php
@@ -170,7 +170,6 @@ class profileActions extends prActions
 					default:
 						break;
 				}
-                //$bc->setCustomLastItem($member->getUsername() . __("'s Profile"));
                 $bc->setCustomLastItem(__("%USERNAME%'s Profile", array('%USERNAME%' => $member->getUsername())));
 				$bc->add(array('name' =>  $member->getUsername() . ',  ' . $member->getAge() . ': ' . $member->getEssayHeadline()));
     }
