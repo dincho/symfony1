adffcffe227edfb807bfce2de8cabf422650809e
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-08-28 13:11:36 +0000

    changed headline TU source in subscription page ( closes #1066 )

--- apps/frontend/modules/subscription/templates/indexSuccess.php
+++ apps/frontend/modules/subscription/templates/indexSuccess.php
@@ -1,6 +1,5 @@
 <?php use_helper('Number', 'dtForm') ?>
-<?php echo __('You are currently using our <strong>free of charge account</strong> that allows to send and receive limited winks and reply to messages.<br />
-In order to see more matches and send messages, please upgrade your subscription below and click save. <br />') ?>
+<?php echo __('Subscription page headline') ?>
 
 <?php $sub1 = $subscriptions[0]; ?>
 <?php echo form_tag('subscription/index', array('id' => 'subscription')) ?>
