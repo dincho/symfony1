be9a5d79216cb58cb3e42a55329b141c077f6fd2
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-08-31 09:14:57 +0000

    added footer menu on "post your story confirmation" page ( closes #1092 )

--- apps/frontend/modules/memberStories/templates/postYourStoryConfirmationSuccess.php
+++ apps/frontend/modules/memberStories/templates/postYourStoryConfirmationSuccess.php
@@ -1,1 +1,5 @@
 <?php echo __('Post your story confirmation body'); ?>
+
+<?php slot('footer_menu') ?>
+    <?php include_partial('content/footer_menu', array('auth' => $sf_user->isAuthenticated())) ?>
+<?php end_slot(); ?>
