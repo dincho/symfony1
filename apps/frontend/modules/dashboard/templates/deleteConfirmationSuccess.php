e78c81890ddbed443a54c85e08967aecdb118d82
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-09-04 11:05:08 +0000

    changed TU source on delete account confirmation page ( closes #1126 )

--- apps/frontend/modules/dashboard/templates/deleteConfirmationSuccess.php
+++ apps/frontend/modules/dashboard/templates/deleteConfirmationSuccess.php
@@ -2,5 +2,5 @@
     <?php echo __('Please tell us why you are deleting your account. ') ?><span> <?php echo __('(optional)') ?></span><br />
     <?php echo textarea_tag('delete_reason', null, array('rows' => 5, 'cols' => 70, 'class' => 'text_area')) ?><br /><br /><br />
     <?php echo button_to(__('Cancel'), 'dashboard/index', array('class' => 'button')) ?>
-    <?php echo submit_tag(__('Delete'), array('class' => 'button')) ?>
+    <?php echo submit_tag(__('Delete Account'), array('class' => 'button')) ?>
 </form>
