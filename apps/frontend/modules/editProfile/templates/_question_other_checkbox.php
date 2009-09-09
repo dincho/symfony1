3c4e7374504574bac96fb29c564d413f9251cb10
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-09-09 14:01:15 +0000

    on registration/selfDescription page input box for other native language is shown only if the checkbox is checked off ( fixes #1084 )

--- apps/frontend/modules/editProfile/templates/_question_other_checkbox.php
+++ apps/frontend/modules/editProfile/templates/_question_other_checkbox.php
@@ -1,5 +1,5 @@
 <?php if( !is_null($question->getOther()) ): ?>
-    <?php echo checkbox_tag('answers['. $question->getid() .']', 'other', fillIn( 'answers['. $question->getid() .']', 'r', false, isset($member_answers[$question->getId()]) && !is_null($member_answers[$question->getId()]->getOther()) ), array('class' => 'zodiac') ) ?>
+    <?php echo checkbox_tag('answers['. $question->getid() .']', 'other', fillIn( 'answers['. $question->getid() .']', 'r', false, isset($member_answers[$question->getId()]) && !is_null($member_answers[$question->getId()]->getOther()) ), array('class' => 'zodiac', 'id' => 'others_check_' . $question->getId()) ) ?>
     <?php echo pr_label_for('answers['. $question->getid() .'][other]', __($question->getOther(ESC_RAW))) ?><br />
     <?php echo input_tag('others['. $question->getid() .']', ( isset($member_answers[$question->getId()]) ) ? $member_answers[$question->getId()]->getOther() : null ) ?>
 <?php endif; ?>
---
+++ web/js/show_hide_other.js
@@ -0,0 +1,12 @@
+window.onload = init;
+
+function show_hide_other()
+{
+  $('others_16').style.display = ( $('others_check_16').checked ) ? '' : 'none';
+}
+
+function init()
+{
+  Event.observe('others_check_16', 'change', show_hide_other );
+  show_hide_other();
+}
