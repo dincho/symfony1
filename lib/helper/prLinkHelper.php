b2ad6df815661e068a464d2a03e4e634afe05322
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-09-10 07:28:39 +0000

    introduced link_to_if_ref and link_to_unless_ref helpers

--- lib/helper/prLinkHelper.php
+++ lib/helper/prLinkHelper.php
@@ -11,4 +11,16 @@ function link_to_ref($name = '', $internal_uri = '', $options = array())
   $options = array_merge($options, $ref);
   return link_to($name, $internal_uri, $options);
 }
+
+function link_to_if_ref($condition, $name = '', $internal_uri = '', $options = array())
+{
+  $ref = array('query_string' => 'return_url=' . urlencode(sfRouting::getInstance()->getCurrentInternalUri()));
+  $options = array_merge($options, $ref);
+  return link_to_if($condition, $name, $internal_uri, $options);
+}
+
+function link_to_unless_ref($condition, $name = '', $url = '', $options = array())
+{
+  return link_to_if_ref(!$condition, $name, $url, $options);
+}
 ?>
