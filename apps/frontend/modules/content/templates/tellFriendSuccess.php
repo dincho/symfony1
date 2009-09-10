0dc4b6fc985e4f37d96516e29d9be3f330b42a0d
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-09-10 12:46:58 +0000

    re-wroked small piece of tell a friend page to comply the prototype which also fixes #1152

--- apps/frontend/modules/content/templates/tellFriendSuccess.php
+++ apps/frontend/modules/content/templates/tellFriendSuccess.php
@@ -1,7 +1,17 @@
 <?php use_helper('dtForm') ?>
 
 <?php echo __('I\'d like tell my friend about PolishRomance.com') ?>
+
 <?php echo form_tag('content/tellFriend', array('id' => 'tell_a_friend_container')) ?>
+    <div id="tell_a_friend_div">
+        <strong><?php echo __('Our Privacy Policy') ?></strong><br /><br />
+        <?php echo __('To assure your friend\'s confidentiality:') ?><br />
+        &bull;&nbsp;<?php echo __('We will only contact your friend once.') ?><br />
+        &bull;&nbsp;<?php echo __('We will not use your friend\'s contact information for any other purpose.') ?><br />
+        &bull;&nbsp;<?php echo __('We will not save your friend\'s contact information.') ?><br />
+        &bull;&nbsp;<?php echo __('We will not forward your friend\'s contact information to any other organization.') ?><br />
+    </div>
+    
     <fieldset>
         <legend style='margin-left: 60px;'><?php echo __('Your Friend\'s Information') ?></legend>
         <?php echo pr_label_for('friend_email', __('Email')) ?>
@@ -21,14 +31,7 @@
         <?php echo pr_label_for('comments', __('Comments <span>(optional)</span>')) ?>
         <?php echo textarea_tag('comments', null, array('rows' => 2, 'cols' => 20)) ?>
     </fieldset>
-    <div id="tell_a_friend_div">
-        <strong><?php echo __('Our Privacy Policy') ?></strong><br /><br />
-        <?php echo __('To assure your friend\'s confidentiality:') ?><br />
-        &bull;&nbsp;<?php echo __('We will only contact your friend once.') ?><br />
-        &bull;&nbsp;<?php echo __('We will not use your friend\'s contact information for any other purpose.') ?><br />
-        &bull;&nbsp;<?php echo __('We will not save your friend\'s contact information.') ?><br />
-        &bull;&nbsp;<?php echo __('We will not forward your friend\'s contact information to any other organization.') ?><br />
-    </div>
+
     <span class="tell_friend_footer">
         <?php echo link_to(__('Cancel and go back to previous page'), 'dashboard/index', array('class' => 'sec_link_small')) ?><br />
         <?php echo submit_tag(__('Send'), array('class' => 'button_mini')) ?>
