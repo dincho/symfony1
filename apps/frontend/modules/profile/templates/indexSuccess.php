6391f68519b0c7135640b1b7b4e5c9e95c754909
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-09-14 10:04:21 +0000

    added check if viewer of the profile page is logged in, to prevent issue when admin preview the profile from backend ( fixes #1179 )

--- apps/frontend/modules/profile/templates/indexSuccess.php
+++ apps/frontend/modules/profile/templates/indexSuccess.php
@@ -7,7 +7,7 @@
         <?php echo link_to_unless($sf_user->getProfile() && $sf_user->getProfile()->hasWinkTo($member->getId()), __('Wink'), 'winks/send?profile_id=' . $member->getId(), 'class=sec_link') ?>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;
         <?php echo link_to(__('Send Mail'), 'messages/send?profile_id=' . $member->getId(), 'class=sec_link') ?>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;
         
-        <?php if( $sf_user->getProfile()->hasInHotlist($member->getId()) ): ?>
+        <?php if( $sf_user->getProfile() && $sf_user->getProfile()->hasInHotlist($member->getId()) ): ?>
            <?php echo link_to_ref(__('Remove from Hotlist'), 'hotlist/delete?profile_id=' . $member->getId(), array('class' => 'sec_link')) ?>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;
         <?php else: ?>
           <?php echo link_to_ref(__('Add to Hotlist'), 'hotlist/add?profile_id=' . $member->getId(), array('class' => 'sec_link')) ?>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;
