c5983361162747014aa24a5d00b86709c011cbda
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-10-05 13:52:43 +0000

    now using static gmap heart to prevent harmless JS errors on profile page ( fixes #1239 )

--- web/js/profile_desc_map.js
+++ web/js/profile_desc_map.js
@@ -10,10 +10,10 @@ function show_profile_desc()
 
 function show_profile_map(address, cityInfo)
 {
-	if( showing_map ) return;
-	
+  if( showing_map ) return;
+  
     if (GBrowserIsCompatible()) {
-    	showing_map = true;
+      showing_map = true;
         document.getElementById('profile_desc').style.display = 'none';
         document.getElementById('profile_map').style.display = '';      
         var map = new GMap2(document.getElementById("gmap"), { size: new GSize(320,320) } );
@@ -24,22 +24,22 @@ function show_profile_map(address, cityInfo)
             function(point) {
                 if (point) {
                     map.setCenter(point, 6); //the second parameter is the zoom
-					var prIcon = new GIcon(G_DEFAULT_ICON);
-					prIcon.shadow = false;
-					prIcon.image = "/images/gmaps_heart.gif";
-					prIcon.iconSize = new GSize(23,21);
-					prIcon.iconAnchor = new GPoint(11, 10); //center the icon over the city
-					//redIcon3.infoWindowAnchor = new GPoint(6,0);
+                    var prIcon = new GIcon(G_DEFAULT_ICON);
+                    prIcon.shadow = false;
+                    prIcon.image = "/images/gmaps_static_heart.gif";
+                    prIcon.iconSize = new GSize(23,21);
+                    prIcon.iconAnchor = new GPoint(11, 10); //center the icon over the city
+                    //redIcon3.infoWindowAnchor = new GPoint(6,0);
       
-					var marker = new GMarker(point, prIcon);
-					map.addOverlay(marker);
-					
-					if( cityInfo )
-					{
-						var label_content = '<div style="padding: 0px 0px 8px 8px; background: url(/images/point_bottom_left.png) no-repeat bottom left;"><div style="background-color: black; padding: 5px; width: 100px">'+ cityInfo + '<\/div><\/div>';
-						var label = new ELabel(point, label_content, null, null, 80, true);
-						map.addOverlay(label);
-					}
+                    var marker = new GMarker(point, prIcon);
+                    map.addOverlay(marker);
+          
+                    if( cityInfo )
+                    {
+                      var label_content = '<div style="padding: 0px 0px 8px 8px; background: url(/images/point_bottom_left.png) no-repeat bottom left;"><div style="background-color: black; padding: 5px; width: 100px">'+ cityInfo + '<\/div><\/div>';
+                      var label = new ELabel(point, label_content, null, null, 80, true);
+                      map.addOverlay(label);
+                    }
                 }
             }
         );
