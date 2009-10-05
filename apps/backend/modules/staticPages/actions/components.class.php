234b2322026ed76ab10f3343298771f357d25799
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-10-05 13:38:23 +0000

    added additional criteria for matching only members with videos when generating best videos

--- apps/backend/modules/staticPages/actions/components.class.php
+++ apps/backend/modules/staticPages/actions/components.class.php
@@ -9,47 +9,52 @@
  
 class staticPagesComponents extends sfComponents
 {
-     public function executeBestVideos()
-     {
-         
-         $lastm = new DateTime($this->get_last_month());
-         $lastmonth = $lastm->format("Y-m");
-         $lastmonth = $lastmonth."%";
-         
-         $c = new Criteria();
-         $c->addJoin(MemberPeer::ID, ProfileViewPeer::PROFILE_ID);
-         $c->add(MemberPeer::YOUTUBE_VID, null, Criteria::ISNOTNULL);
-         $criteria = $c->getNewCriterion(ProfileViewPeer::CREATED_AT, $lastmonth, Criteria::LIKE);
-         $c->addAnd($criteria);
-         $c->addGroupByColumn(ProfileViewPeer::PROFILE_ID);
-         $c->addDescendingOrderByColumn('COUNT(profile_view.profile_id)');
-         $c->setLimit(3);
-         
-         $members = MemberPeer::doSelect($c);
-		 
-		 $cc = new Criteria();
-         $cc->add(BestvTmplI18nPeer::CULTURE, $this->culture);
-         $template = BestvTmplI18nPeer::doSelectOne($cc);
-         
-         $content = str_replace('{WINNER}', ($members[0]->getFirstName() ." ". $members[0]->getLastName()), $template->getHeader());
-         $i=1;
-         foreach ($members as $member) {
-         	$tmpl_name=array('{FULL_NAME}', '{USERNAME}', '{YOUTUBE}', '{N}');
-         	$source_name=array(($member->getFirstName() ." ". $member->getLastName()), $member->getUsername(), $member->getYoutubeVid(), $i);
-            $content .= str_replace($tmpl_name, $source_name, $template->getBodyWinner());
-            
-            $i++;
-         }
-         $content .= $template->getFooter();
+  public function executeBestVideos()
+  {
+    $lastm = new DateTime($this->get_last_month());
+    $lastmonth = $lastm->format("Y-m");
+    $lastmonth = $lastmonth."%";
 
-         $this->content = $content;
-            
-     }
+    $c = new Criteria();
+    $c->addJoin(MemberPeer::ID, ProfileViewPeer::PROFILE_ID);
+    $crit = $c->getNewCriterion(MemberPeer::YOUTUBE_VID, NULL, Criteria::ISNOTNULL);
+    $crit->addAnd($c->getNewCriterion(MemberPeer::YOUTUBE_VID, '', Criteria::NOT_EQUAL));
+    $c->add($crit);
+    $criteria = $c->getNewCriterion(ProfileViewPeer::CREATED_AT, $lastmonth, Criteria::LIKE);
+    $c->addAnd($criteria);
+    $c->addGroupByColumn(ProfileViewPeer::PROFILE_ID);
+    $c->addDescendingOrderByColumn('COUNT(profile_view.profile_id)');
+    $c->setLimit(3);
+
+    $members = MemberPeer::doSelect($c);
+    if( count($members) > 0 )
+    {
+      $cc = new Criteria();
+      $cc->add(BestvTmplI18nPeer::CULTURE, $this->culture);
+      $template = BestvTmplI18nPeer::doSelectOne($cc);
+
+      $content = str_replace('{WINNER}', ($members[0]->getFirstName() ." ". $members[0]->getLastName()), $template->getHeader());
+      $i=1;
+      foreach ($members as $member)
+      {
+        $tmpl_name=array('{FULL_NAME}', '{USERNAME}', '{YOUTUBE}', '{N}');
+        $source_name=array(($member->getFirstName() ." ". $member->getLastName()), $member->getUsername(), $member->getYoutubeVid(), $i);
+        $content .= str_replace($tmpl_name, $source_name, $template->getBodyWinner());
+
+        $i++;
+      }
+      $content .= $template->getFooter();
+    } else {
+      $content = "!!!NOT ENOUGH DATA TO GENERATE RESULTS!!!";
+    }
+
+    $this->content = $content;
+  }
      
-     public static function get_last_month() 
-     {
-        return (date('m') == 1)?
-        join('-', array(date('Y')-1, '12')) :
-        join('-', array(date('Y'), date('m')-1)) ;
-     }
+  public static function get_last_month() 
+  {
+    return (date('m') == 1)?
+    join('-', array(date('Y')-1, '12')) :
+    join('-', array(date('Y'), date('m')-1)) ;
+  }
 }
