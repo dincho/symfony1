ebc7e826feb2ef59cf959f64356afe76cc494a54
Dincho <Dincho@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2009-10-12 12:57:58 +0000

    fixed images issue on homepage when beta period is disabled

--- apps/frontend/modules/content/templates/_homepagePhotoSet.php
+++ apps/frontend/modules/content/templates/_homepagePhotoSet.php
@@ -1,10 +1,10 @@
 <?php foreach($photos as $photo): ?>
     <?php if( sfConfig::get('app_beta_period') ): ?>
-        <?php echo link_to(image_tag($photo->getTiltImageUrlPath()), 'registration/joinNow') ?>
+        <?php echo link_to(image_tag($photo->getTiltImageUrlPath(), array('size' => '100x95')), 'registration/joinNow') ?>
     <?php else: ?>
         <?php $looking_for = ( $photo->getGender() == 'M') ? 'F_M' : 'M_F'; ?>
         
         <?php echo link_to(image_tag($photo->getTiltImageUrlPath(), array('size' => '100x95')), 
-													'search/public?filter=filter&filters[looking_for]=' . $looking_for, array( 'class' => 'highlighted_link')) ?>
+                          'search/public?filter=filter&filters[looking_for]=' . $looking_for, array( 'class' => 'highlighted_link')) ?>
     <?php endif; ?>
 <?php endforeach; ?>
