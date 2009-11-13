f45e732b3b2187ec2f794948e5ffe320ca3950a0
dincho <dincho@xbuv.com>
2009-11-13 14:20:11 +0000

    internationalized dates on manage subscription page ( closes #1295 )

--- apps/frontend/modules/subscription/templates/manageSuccess.php
+++ apps/frontend/modules/subscription/templates/manageSuccess.php
@@ -1,19 +1,19 @@
-<?php use_helper('Number') ?>
+<?php use_helper('Number', 'Date') ?>
 <?php echo __('Manage subscription - membership status'); ?>
 
 <?php if( $member->getLastPaypalItem() == 'membership'): //normal membership by member ?>
     <?php if( is_null($member->getPaypalUnsubscribedAt()) ): //still not canceled subscription ?>
         <?php echo __('Manage subscription - auto-renewal', 
-                      array('%EOT_DATE%' => date('M d, Y', $member->getEotDate()),
-                            '%NEXT_PAYMENT_DATE%' => date('M d, Y', $member->getNextPaymentDate()),
+                      array('%EOT_DATE%' => format_date($member->getEotDate(null), 'MMM dd, yyyy'),
+                            '%NEXT_PAYMENT_DATE%' => format_date($member->getNextPaymentDate(null), 'MMM dd, yyyy'),
                             '%NEXT_PAYMENT_AMOUNT%' => format_currency($member->getNextPaymentAmount(), 'GBP'),
                       )); ?>
         <?php echo button_to(__('Unsubscribe'), sfConfig::get('app_paypal_url') . '?cmd=_subscr-find&alias=' . urlencode(sfConfig::get('app_paypal_business')), array('popup' => true, 'class' => 'button')) ?>    
     <?php else: ?>
-         <?php echo __('Manage subscription - subscription canceled', array('%EOT_DATE%' => date('M d, Y', $member->getEotDate()))); ?>
+         <?php echo __('Manage subscription - subscription canceled', array('%EOT_DATE%' => format_date($member->getEotDate(null), 'MMM dd, yyyy'))); ?>
     <?php endif; ?>
 <?php elseif( $member->getLastPaypalItem() == 'gift_membership' ): //gift membership ?>
-    <?php echo __('Manage subscription - gift membership', array('%EOT_DATE%' => date('M d, Y', $member->getEotDate())) ); ?>
+    <?php echo __('Manage subscription - gift membership', array('%EOT_DATE%' => format_date($member->getEotDate(null), 'MMM dd, yyyy')) ); ?>
 <?php else: ?>
     <?php echo __('Manage subscription - membership by admin'); ?>
 <?php endif; ?>
