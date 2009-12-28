32255b475e732ee132abf269b93291cdfbdf945e
dincho <dincho@xbuv.com>
2009-12-28 14:08:36 +0000

    [backend] re-ordered input fields on edit homepage, it should be OK with minimum resolution of 1280x1024 ( closes #1437 )

--- apps/backend/modules/content/templates/homepageSuccess.php
+++ apps/backend/modules/content/templates/homepageSuccess.php
@@ -4,25 +4,10 @@
 <?php echo form_tag('content/homepage', 'class=form') ?>
   <?php echo input_hidden_tag('culture', $culture, 'class=hidden') ?>
   <div class="legend">Edit Home Page</div>
-  
-      <fieldset class="form_fields float-right">
-        <label for="footer_articles">Footer Articles:</label><br />
-        <?php echo textarea_tag('trans[148]', (isset($trans[148])) ? $trans[148]->getTarget() : null, 'cols=120 rows=28 ' . error_class('footer_articles')) ?><br />
-      </fieldset>  
-        
+
       <fieldset class="form_fields">
-      
         <label for="culture">Language</label>
         <var><?php echo format_language($culture) ?></var><br />
-        
-        <label for="tagline">Tagline</label>
-        <?php echo textarea_tag('trans[1]', (isset($trans[1])) ? $trans[1]->getTarget() : null, array('cols' => 40, 'rows' => 2)) ?><br />
-        
-        <label for="introduction">Introduction</label>
-        <?php echo textarea_tag('trans[2]', (isset($trans[2])) ? $trans[2]->getTarget() : null, array('cols' => 40, 'rows' => 5)) ?><br />
-    
-      </fieldset>
-      <fieldset class="form_fields">
         
         <label for="title">Title:</label>
         <?php echo input_tag('trans[3]', (isset($trans[3])) ? $trans[3]->getTarget() : null, error_class('title')) ?><br /> 
@@ -33,12 +18,27 @@
         <label for="description">Description:</label>
         <?php echo textarea_tag('trans[5]', (isset($trans[5])) ? $trans[5]->getTarget() : null, 'cols=26 rows=2 ' . error_class('description')) ?><br />
       </fieldset>
+
+      
+      <fieldset class="form_fields float-left" style="margin-right: 45px">
+        <label for="tagline">Tagline</label>
+        <?php echo textarea_tag('trans[1]', (isset($trans[1])) ? $trans[1]->getTarget() : null, array('cols' => 40, 'rows' => 2)) ?><br />
+        
+        <label for="introduction">Introduction</label>
+        <?php echo textarea_tag('trans[2]', (isset($trans[2])) ? $trans[2]->getTarget() : null, array('cols' => 40, 'rows' => 5)) ?><br />
+      </fieldset>
+        
   
       <fieldset class="form_fields">
         <label>Member Stories</label>
         <?php echo select_tag('member_stories', objects_for_select($member_stories, 'getId', 'getTitle', $homepage_stories), array('multiple' => true, 'style' => 'width: 350px; height: 200px')) ?>
-      </fieldset>
+      </fieldset>        
+
       
+      <fieldset class="form_fields">
+        <label for="footer_articles">Footer Articles:</label>
+        <?php echo textarea_tag('trans[148]', (isset($trans[148])) ? $trans[148]->getTarget() : null, 'cols=120 rows=28 ' . error_class('footer_articles')) ?><br />
+      </fieldset>        
 
   
   <fieldset class="actions">
