566e573816ede9ed673a41d20499e5624d727178
dincho <dincho@xbuv.com>
2010-01-11 16:28:02 +0000

    changed form fields styling "on focus" to black color on white background ( closes #1430 )

--- web/js/active_fields.js
+++ web/js/active_fields.js
@@ -1,20 +1,23 @@
 Event.observe(window, 'load', function() {
   var fields = $$("input");
   for (var i = 0; i < fields.length; i++) {
-    fields[i].onfocus = function()
+    if( fields[i].type != 'submit' )
     {
-    	this.className += ' focused';
-    	if(typeof global_focus_func == 'function') {
-    		global_focus_func(this);
-		}     	
-	}
-    fields[i].onblur = function()
-    {
-    	this.className = this.className.replace('focused', '');
-    	
-    	if(typeof global_focus_func == 'function') {
-    		global_focus_func(this, true);
-		}     	
-	}
+        fields[i].onfocus = function()
+        {
+            this.className += ' focused';
+            if(typeof global_focus_func == 'function') {
+                global_focus_func(this);
+            }         
+        }
+        fields[i].onblur = function()
+        {
+            this.className = this.className.replace('focused', '');
+        
+            if(typeof global_focus_func == 'function') {
+                global_focus_func(this, true);
+            }         
+        }
+    }
   }
 });
