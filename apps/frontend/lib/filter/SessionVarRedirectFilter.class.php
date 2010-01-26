16c9bcf823bf40e51fb7bdc4ac1ab2044c4d8ef6
dincho <dincho@xbuv.com>
2010-01-26 14:15:26 +0000

    added session var redirect filter ( closes #1427 )

---
+++ apps/frontend/lib/filter/SessionVarRedirectFilter.class.php
@@ -0,0 +1,26 @@
+<?php
+class SessionVarRedirectFilter extends sfFilter
+{
+
+    public function execute($filterChain)
+    {
+        if ($this->isFirstCall())
+        {
+            $context = $this->getContext();
+
+            $request = $this->getContext()->getRequest();
+
+            if( $request->getParameter('PRSSID') )
+            {
+                $uri = sfRouting::getInstance()->getCurrentInternalUri();
+                $uri = preg_replace('#\?PRSSID=.*#', '', $uri);
+
+                $AI = $context->getActionStack()->getLastEntry()->getActionInstance();
+                $AI->redirect($uri);
+            }
+            
+        }
+        
+        $filterChain->execute();
+    }
+}
