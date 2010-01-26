a2321eef8626d5df2b60c5f8ec32a0e3d9e2aa33
dincho <dincho@xbuv.com>
2010-01-26 14:14:01 +0000

    ads dimension corrections to member story page ( closes #1486 )

--- apps/frontend/modules/memberStories/templates/readSuccess.php
+++ apps/frontend/modules/memberStories/templates/readSuccess.php
@@ -31,6 +31,7 @@
             google_ad_slot = "3396883494";
             google_ad_width = 160;
             google_ad_height = 600;
+
         //-->
         </script>
         <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
