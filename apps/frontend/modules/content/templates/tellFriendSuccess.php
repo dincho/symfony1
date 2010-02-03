19c4da632f65b49c83f9b7caf799f2742e8449a1
dincho <dincho@xbuv.com>
2010-02-03 10:36:41 +0000

    simplified tell a friend form - removed all fields instead of 2 emails, removed validation some css enhancements ( closes #1504 )

--- apps/frontend/modules/content/templates/tellFriendSuccess.php
+++ apps/frontend/modules/content/templates/tellFriendSuccess.php
@@ -2,40 +2,29 @@
 
 <?php echo __('I\'d like tell my friend about PolishRomance.com') ?>
 
-<?php echo form_tag('content/tellFriend', array('id' => 'tell_a_friend_container')) ?>
-    <div id="tell_a_friend_div">
-        <strong><?php echo __('Our Privacy Policy') ?></strong><br /><br />
-        <?php echo __('To assure your friend\'s confidentiality:') ?><br />
-        &bull;&nbsp;<?php echo __('We will only contact your friend once.') ?><br />
-        &bull;&nbsp;<?php echo __('We will not use your friend\'s contact information for any other purpose.') ?><br />
-        &bull;&nbsp;<?php echo __('We will not save your friend\'s contact information.') ?><br />
-        &bull;&nbsp;<?php echo __('We will not forward your friend\'s contact information to any other organization.') ?><br />
-    </div>
-    
+<?php echo form_tag('content/tellFriend', array('id' => 'tell_a_friend')) ?>
     <fieldset>
-        <legend style='margin-left: 60px;'><?php echo __('Your Friend\'s Information') ?></legend>
-        <?php echo pr_label_for('friend_email', __('Email')) ?>
+        <div class="form_info">
+            <strong><?php echo __('Our Privacy Policy') ?></strong><br /><br />
+            <?php echo __('To assure your friend\'s confidentiality:') ?><br />
+            &bull;&nbsp;<?php echo __('We will only contact your friend once.') ?><br />
+            &bull;&nbsp;<?php echo __('We will not use your friend\'s contact information for any other purpose.') ?><br />
+            &bull;&nbsp;<?php echo __('We will not save your friend\'s contact information.') ?><br />
+            &bull;&nbsp;<?php echo __('We will not forward your friend\'s contact information to any other organization.') ?><br />
+        </div>
+        
+        <?php echo pr_label_for('friend_email', __('Your Friend Email')) ?>
         <?php echo input_tag('friend_email') ?><br />
         
-        <?php echo pr_label_for('friend_full_name', __('Full Name')) ?>
-        <?php echo input_tag('friend_full_name') ?>
-    </fieldset>
-    <fieldset>
-        <legend style='margin-left: 60px;'><?php echo __('Your Information') ?></legend>
-        <?php echo pr_label_for('email', __('Email')) ?>
+        <?php echo pr_label_for('email', __('Your email')) ?>
         <?php echo input_tag('email') ?><br />
-        
-        <?php echo pr_label_for('full_name', __('Full Name')) ?>
-        <?php echo input_tag('full_name') ?><br />
-        
-        <?php echo pr_label_for('comments', __('Comments <span>(optional)</span>')) ?>
-        <?php echo textarea_tag('comments', null, array('rows' => 2, 'cols' => 20)) ?>
     </fieldset>
 
-    <span class="tell_friend_footer">
+    <fieldset class="form_actions">
         <?php echo link_to(__('Cancel and go back to previous page'), 'dashboard/index', array('class' => 'sec_link_small')) ?><br />
         <?php echo submit_tag(__('Send'), array('class' => 'button_mini')) ?>
-    </span>
+    </fieldset>
+    
 </form>
 
 <?php slot('footer_menu') ?>
