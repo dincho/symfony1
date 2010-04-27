d33ee14a05fd63af0b1f841655d89a6e987718b7
dincho <dincho@xbuv.com>
2010-04-27 08:12:08 +0000

    2 new TU's and buttons relocation on subscription payment page ( closes #1675 )

--- apps/frontend/modules/subscription/templates/paymentSuccess.php
+++ apps/frontend/modules/subscription/templates/paymentSuccess.php
@@ -1,34 +1,36 @@
 <?php use_helper('Number', 'Javascript') ?>
 <?php echo __('You need to pay %AMOUNT%', array('%AMOUNT%' => format_currency($amount, sfConfig::get('app_settings_currency_' . $sf_user->getCulture(), 'GBP')))) ?><br /><br /><br />
 
-<?php echo form_tag(sfConfig::get('app_paypal_url'), array('method' => 'post')) ?>
+<?php echo form_tag(sfConfig::get('app_paypal_url'), array('method' => 'post', 'id' => 'paypal_form', )) ?>
     <?php echo input_hidden_tag('cmd', '_s-xclick') , "\n" ?>
 <input type="hidden" name="encrypted" value="
 -----BEGIN PKCS7-----
 <?php echo $encrypted . "\n" ?>
 -----END PKCS7-----
 ">
-    <?php echo link_to(__('Cancel and return to subscription'), 'subscription/index', array('class' => 'sec_link_small')) ?><br />
-    <fieldset id="payment_buttons">
-        <?php echo submit_tag(__('Subscribe With PayPal'), array('class' => 'button')) ?>
-        <?php if( $zongAvailable ): ?>
-            <?php echo button_to_function(__('Pay with Zong+'), 'show_zong_payment();', array('class' => 'button')); ?>
-        <?php endif; ?>
-    </fieldset>
+    <?php echo submit_tag(__('Subscribe With PayPal'), array('class' => 'button', 'id' => 'paypal_button')) ?>
 </form>
-
+<?php echo __('Payment description - paypal'); ?>
 
 
 <?php if( $zongAvailable ): ?>
+
+    <?php echo button_to_function(__('Pay with Zong+'), 'show_zong_payment();', array('class' => 'button', 'id' => 'zong_button')); ?>
+    <?php echo __('Payment description - zong'); ?>
+    
     <div id="zongPayment_container" style="display: none;"><?php echo __('Loading please wait ...'); ?></div>
 
     <script type="text/javascript" charset="utf-8">
         function show_zong_payment()
         {
-            $('payment_buttons').hide();
+            $('paypal_button').hide();
+            $('zong_button').hide();
             $('zongPayment_container').show();
         
             <?php echo remote_function(array('update' => 'zongPayment_container', 'url' => 'ajax/zongPayment?msid=' . $member_subscription_id)); ?>
         }
     </script>
 <?php endif; ?>
+
+<br />
+<?php echo link_to(__('Cancel and return to subscription'), 'subscription/index', array('class' => 'sec_link_small')) ?>
