79564db4cc485f19c218c5c6fb99ff01a8fdc920
dincho <dincho@xbuv.com>
2010-04-30 10:14:05 +0000

    fixed updateMatches high memory usage ( refs #1683 )

--- batch/cronjob/updateMatches.php
+++ batch/cronjob/updateMatches.php
@@ -14,8 +14,8 @@
 
 define('SF_ROOT_DIR',    realpath(dirname(__file__).'/../..'));
 define('SF_APP',         'backend');
-define('SF_ENVIRONMENT', 'dev');
-define('SF_DEBUG',       1);
+define('SF_ENVIRONMENT', 'prod');
+define('SF_DEBUG',       0);
 set_time_limit(0);
 
 require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
@@ -29,19 +29,38 @@ $databaseManager->initialize();
 // batch process here
 
 
-//echo 'Initial memory: ' . number_format(memory_get_usage(), 0, '.', ',') . " bytes\n";
+//echo 'Initial memory: ' . number_format(memory_get_usage()/1024/1024, 0, '.', ',') . " MB\n";
 
 $c = new Criteria();
-$members = MemberPeer::doSelect($c);
-unset($c);
 
-foreach( $members as $member )
+$page = 0;
+$per_page = 100;
+$total_cnt = 0;
+
+$pager = new sfPropelPager('Member', $per_page);
+$pager->setCriteria($c);
+
+do
 {
-  $member->updateMatches();
+    $page++;
+    $pager->setPage($page);
+    $pager->init();
+    
+    //printf("Page: %d, set %d \n", $page, $page*$per_page);
+    
+    foreach( $pager->getResults() as $member )
+    {
+      $member->updateMatches();
+      $total_cnt++;
+    }
+ 
 }
+while( $page != $pager->getLastPage() );
 
-// echo 'Peak: ' . number_format(memory_get_peak_usage(), 0, '.', ',') . " bytes\n";
-// echo 'End: ' . number_format(memory_get_usage(), 0, '.', ',') . " bytes\n";
+
+// echo "Total processed: " . $total_cnt . "\n";
+// echo 'Peak: ' . number_format(memory_get_peak_usage()/1024/1024, 0, '.', ',') . " MB\n";
+// echo 'End: ' . number_format(memory_get_usage()/1024/1024, 0, '.', ',') . " MB\n";
 // 
 // $total_time = sprintf('%.7f', (microtime(true) - sfConfig::get('pr_timer_start')));
 // echo 'Execution time: ' . $total_time . "\n";
