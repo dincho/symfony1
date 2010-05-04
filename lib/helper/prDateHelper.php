9401ea7c1c72d1545060cd7edb538ea3e15bcd5a
dincho <dincho@xbuv.com>
2010-05-04 07:20:00 +0000

    datetime formatting helper fix - timezone issues ( fixes #1687 )

--- lib/helper/prDateHelper.php
+++ lib/helper/prDateHelper.php
@@ -1,39 +1,33 @@
 <?php
 function format_date_pr($time = null, $time_format = null, $date_format = null, $timezone = null)
 {
-    use_helper('Date');
-    use_helper('I18N');
-        
+    sfLoader::loadHelpers(array('I18N'));
+
     $culture = sfContext::getInstance()->getUser()->getCulture();
     $today = time();
     $yesterday = $today - 86400;
-        
-    if( is_null($time_format) ) $time_format = ($culture == 'en') ? ', hh:mm a' : ', HH:mm';
-    if( is_null($time) ) $time = $today;
-    if( is_null($date_format) ) $date_format = 'dd MMMM';
-
-    if( !is_null($timezone) )
-    {
-        $newTimezone = new DateTimeZone($timezone);
-        $dateTime = new DateTime("@$time");
-        $offset = $newTimezone->getOffset($dateTime);
-        $time = $time+$offset;
-    }
     
-    if( date('d', $time) == date('d', $today) && date('m', $time) == date('m', $today) && date('Y', $time) == date('Y', $today) )
+    if( is_null($time) ) $time = $today;
+    if( is_null($time_format) ) $time_format = ($culture == 'en') ? ', h:i A' : ', H:i';
+    if( is_null($date_format) ) $date_format = 'd F';
+    
+    $dateTime = new DateTime("@$time");
+    if( !is_null($timezone) ) $dateTime->setTimezone(new DateTimeZone($timezone));
+    
+    if( $dateTime->format('dmY') == date('dmY', $today) )
     {
       $string = __('Today');
-    } elseif ( date('d', $time) == date('d', $yesterday) && date('m', $time) == date('m', $yesterday) && date('Y', $time) == date('Y', $yesterday) )
+    } elseif ( $dateTime->format('dmY') == date('dmY', $yesterday) )
     {
       $string = __('Yesterday');
     } else {
-      $string = format_date($time, $date_format);
+      $string = $dateTime->format($date_format);
     }
 
-    if( $time_format && date('Y', $time) != date('Y') ) $time_format = ($culture == 'en') ?  ', yyyy hh:mm a' : ', yyyy HH:mm';
+    if( $time_format && $dateTime->format('Y') != date('Y', $today) ) $time_format = ($culture == 'en') ?  ', Y h:i A' : ', Y H:i';
 
     //add the time
-    if($time_format) $string .= format_date($time, $time_format);
+    if($time_format) $string .= $dateTime->format($time_format);
 
     return $string;
 }
