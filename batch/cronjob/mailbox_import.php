bf0c742cc7673aa4aa58308945937e2490dfcbf3
dincho <dincho@xbuv.com>
2010-05-20 10:31:31 +0000

    now all batches are executed in prod environment ( closes #1692 )

--- batch/cronjob/mailbox_import.php
+++ batch/cronjob/mailbox_import.php
@@ -10,8 +10,8 @@
  */
 define('SF_ROOT_DIR',    realpath(dirname(__file__).'/../..'));
 define('SF_APP', 'backend');
-define('SF_ENVIRONMENT', 'dev');
-define('SF_DEBUG', 1);
+define('SF_ENVIRONMENT', 'prod');
+define('SF_DEBUG',       0);
 require_once (SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'apps' . DIRECTORY_SEPARATOR . SF_APP . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php');
 // initialize database manager
 $databaseManager = new sfDatabaseManager();
