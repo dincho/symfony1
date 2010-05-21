6b3aba370241f96423eba238c41aba4e87f4780f
dincho <dincho@xbuv.com>
2010-05-21 13:24:08 +0000

    just test

--- config/config.php
+++ config/config.php
@@ -4,5 +4,4 @@ $sf_symfony_lib_dir  = dirname(__FILE__).'/../lib/symfony';
 $sf_symfony_data_dir = dirname(__FILE__).'/../data/symfony';
 
 if( !defined('SALT') ) define('SALT', 'fj`Jbu6`r7XFvC$V@<%3l#lE/K/J\;Ai');
-
 ini_set('session.gc_maxlifetime', 1800); // 30 minutes
