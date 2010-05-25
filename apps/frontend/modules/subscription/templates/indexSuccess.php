3442c5941411237d5a92ebc1e22938bb422479aa
dincho <dincho@xbuv.com>
2010-05-25 15:55:02 +0000

    subscription page visual changes ( closes #1717 )

--- apps/frontend/modules/subscription/templates/indexSuccess.php
+++ apps/frontend/modules/subscription/templates/indexSuccess.php
@@ -6,55 +6,58 @@
 
 <?php echo form_tag('subscription/index', array('id' => 'subscription')) ?>
     <fieldset style="width: 90%;">
-        <div style='float: left; margin-top: 44px; width: 1px; height: 266px; border-right: 1px solid #3D3D3D;'></div>
+        <div style='float: left; margin-top: 44px; width: 1px; height: 294px; border-right: 1px solid #3D3D3D;'></div>
         <div class="column" >
             <div class="upgrade_header">&nbsp;</div>
-            <span class="top_one">&nbsp;</span>
-            <span class="type"><?php echo __('Create a profile') ?></span><br />
-            <span class="type"><?php echo __('Post a photo') ?></span><br />
-            <span class="type"><?php echo __('Send and receive winks') ?></span><br />
-            <span class="type"><?php echo __('Respond to messages') ?></span><br />
-            <span class="type"><?php echo __('Send messages') ?></span><br />
-            <span class="type"><?php echo __('See who\'s viewed your profile') ?></span><br />
-            <span class="type"><?php echo __('Contact Online Assistant') ?></span><br />
-            <span class="type"><?php echo __('Private Dating') ?></span><br />
-            <span class="type">
-                <?php echo __('Fee per %PERIOD% ' . pr_format_payment_period_type($sub1->getPeriodType()), array('%PERIOD%' => $sub1->getPeriod())) ?>
-            </span><br />
-            <span class="select_one"><?php echo __('Select Membership') ?></span>
+            <div class="subscription_features">
+              <span class="top">&nbsp;</span>
+              <span class="type"><?php echo __('Create a profile') ?></span><br />
+              <span class="type"><?php echo __('Post a photo') ?></span><br />
+              <span class="type"><?php echo __('Send and receive winks') ?></span><br />
+              <span class="type"><?php echo __('Respond to messages') ?></span><br />
+              <span class="type"><?php echo __('Send messages') ?></span><br />
+              <span class="type"><?php echo __('See who\'s viewed your profile') ?></span><br />
+              <span class="type"><?php echo __('Contact Online Assistant') ?></span><br />
+              <span class="type"><?php echo __('Private Dating') ?></span><br />
+              <span class="type">
+                  <?php echo __('Fee per %PERIOD% ' . pr_format_payment_period_type($sub1->getPeriodType()), array('%PERIOD%' => $sub1->getPeriod())) ?>
+              </span><br />
+              <span class="select"><?php echo __('Select Membership') ?>&nbsp;</span>
+            </div>
         </div>
         <?php foreach($subscriptions as $subscription): ?>
             <?php $is_better = ( $subscription->getAmount() > $member->getSubscription()->getAmount()) ? true : false; ?>
-            <div class="column <?php if($is_better && !$better_subscription_flag ) echo 'upgrade_to' ?>">
+            <div class="column <?php if( $subscription->getAmount() > 0 ) echo 'upgrade_to' ?>">
                 <div class="upgrade_header">
-                  <?php if( $is_better && !$better_subscription_flag ): ?>
-                    <?php echo __('Upgrade to %SUBSCRIPTION_TITLE% account!', array('%SUBSCRIPTION_TITLE%' => $subscription->getTitle())); ?>
+                  <?php if( $is_better  ): ?>
+                    <div><?php echo __('Upgrade to %SUBSCRIPTION_TITLE% account!', array('%SUBSCRIPTION_TITLE%' => $subscription->getTitle())); ?></div>
                   <?php else: ?>
                     &nbsp;
                   <?php endif; ?>
                 </div>
-                <span class="top"><?php echo __($subscription->getTitle()) ?></span>
-                <span class="check"><?php echo image_tag('check_mark.gif') ?></span>
-                <span class="check"><?php echo ($subscription->getCanPostPhoto()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
-                <span class="check"><?php echo ($subscription->getCanWink()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
-                <span class="check"><?php echo ($subscription->getCanReplyMessages()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
-                <span class="check"><?php echo ($subscription->getCanSendMessages()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
-                <span class="check"><?php echo ($subscription->getCanSeeViewed()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
-                <span class="check"><?php echo ($subscription->getCanContactAssistant()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
-                <span class="check"><?php echo ($subscription->getPrivateDating()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
-                <span class="check"><?php echo ($subscription->getAmount() > 0 ) ? format_currency($subscription->getAmount(), sfConfig::get('app_settings_currency_' . $sf_user->getCulture(), 'GBP')) : __('Free')?></span>
-                <span class="select">
-                  <?php if( $is_better): ?>
-                    <?php $better_subscription_flag = true; ?>
-                    <?php echo button_to(__('Upgrade to %SUBSCRIPTION_TITLE%',  array('%SUBSCRIPTION_TITLE%' => $subscription->getTitle())), 
-                                        'subscription/payment?sid=' . $subscription->getId(), array('class' => 'button')); ?>
-                  <?php else: ?>
-                    &nbsp;
-                  <?php endif; ?>
-                </span>
+                <div class="<?php echo ( $subscription->getAmount() > 0 ) ? 'outlined_subscription_features' : 'subscription_features';?>">
+                  <span class="top"><?php echo __($subscription->getTitle()) ?></span>
+                  <span class="check"><?php echo image_tag('check_mark.gif') ?></span>
+                  <span class="check"><?php echo ($subscription->getCanPostPhoto()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
+                  <span class="check"><?php echo ($subscription->getCanWink()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
+                  <span class="check"><?php echo ($subscription->getCanReplyMessages()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
+                  <span class="check"><?php echo ($subscription->getCanSendMessages()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
+                  <span class="check"><?php echo ($subscription->getCanSeeViewed()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
+                  <span class="check"><?php echo ($subscription->getCanContactAssistant()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
+                  <span class="check"><?php echo ($subscription->getPrivateDating()) ? image_tag('check_mark.gif') : '&nbsp;'?></span>
+                  <span class="check"><?php echo ($subscription->getAmount() > 0 ) ? format_currency($subscription->getAmount(), sfConfig::get('app_settings_currency_' . $sf_user->getCulture(), 'GBP')) : __('Free')?></span>
+                  <span class="select">
+                    <?php if( $is_better): ?>
+                      <?php $better_subscription_flag = true; ?>
+                      <?php echo button_to(__('Upgrade to %SUBSCRIPTION_TITLE%',  array('%SUBSCRIPTION_TITLE%' => $subscription->getTitle())), 
+                                          'subscription/payment?sid=' . $subscription->getId(), array('class' => 'button')); ?>
+                    <?php else: ?>
+                      &nbsp;
+                    <?php endif; ?>
+                  </span>
+                </div>
             </div>        
         <?php endforeach; ?>
-    <div style='float: left; margin-top: 44px; width: 1px; height: 266px; border-left: 1px solid #3D3D3D;'></div>
     </fieldset>
     <br class="clear" />
     <br /><br /><br /><?php echo __('Please allow up at 48 hours for the changes to take effect.') ?><br />
