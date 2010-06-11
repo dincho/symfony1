a5e62235836057da8951e8ba3383203d37c84698
vvidov <vvidov@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2010-06-11 06:48:13 +0000

    save date_modified in catalogue table used in cache update sfMessageSource_MySQL::getLastModified
    (fixes #1729)

--- lib/model/TransUnit.php
+++ lib/model/TransUnit.php
@@ -9,4 +9,17 @@
  */ 
 class TransUnit extends BaseTransUnit
 {
+    public function save($con = null)
+    {
+
+      if( $this->isModified() )
+      {
+        $this->setDateModified(time());
+        //save date_modified in catalogue table
+        // used in cache update sfMessageSource_MySQL::getLastModified
+        $this->getCatalogue()->setDateModified($this->getDateModified());
+      }
+      parent::save();
+    }
+
 }
