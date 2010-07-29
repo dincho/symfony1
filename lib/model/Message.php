057cad77fef75d03a30ba0b9a438875a38fbece1
dincho <dincho@xbuv.com>
2010-07-29 07:02:59 +0000

    cleanup

--- lib/model/Message.php
+++ lib/model/Message.php
@@ -20,31 +20,7 @@ class Message extends BaseMessage
     {
         return MemberPeer::retrieveByPK($this->getToMemberId());
     }
-    
-    // public function save($con = null)
-    // {
-    //     if( $this->isNew() && !$this->getSentBox() && !$this->getIsSystem() && parent::save($con))
-    //     {
-    //         $from_member = $this->getMemberRelatedByFromMemberId();
-    //         $to_member = $this->getMemberRelatedByToMemberId();
-    //         
-    //         if( $this->is_reply )
-    //         {
-    //             $from_member->incCounter('ReceivedMessages');
-    //             $to_member->incCounter('ReplyMessages');
-    //         } else {
-    //             if( $from_member->getNbSendMessagesToday()+1 == sfConfig::get('app_settings_notification_spam_msgs') ) 
-    //                 Events::triggerSpamActivity($from_member,$from_member->getNbSendMessagesToday()+1);
-    //             
-    //             $from_member->incCounter('SentMessages');
-    //             $to_member->incCounter('ReceivedMessages');
-    //         }
-    //         return true;
-    //     }
-    //     
-    //     return parent::save($con);
-    // }
-    
+
     public function reply($subject = '', $content = '', $draft_id = null)
     {
         $send_message = MessagePeer::send($this->getMemberRelatedByToMemberId(), 
@@ -76,10 +52,7 @@ class Message extends BaseMessage
                                       '%MESSAGE%' => $message_body,
                                       ));
                                       
-        //$ret .= 'On ' . $this->getCreatedAt('M d, Y H:i A, ') .  $from . ' wrote: ' . "\r\n";
         return $ret;
     }
     
-
-        
 }
