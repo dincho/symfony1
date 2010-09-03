3b8fd531a4f12036600f4e58591bacf17d4cb16c
dincho <dincho@xbuv.com>
2010-09-03 13:24:58 +0000

    preventing batch jobs from run when environment is disabled ( fixes #1972 )

--- batch/config.php
+++ batch/config.php
@@ -4,4 +4,8 @@ define('SF_APP',         'backend');
 define('SF_ENVIRONMENT', 'dev');
 define('SF_DEBUG',       1);
 
+$lock_file = SF_ROOT_DIR.DIRECTORY_SEPARATOR.SF_APP.'_'.SF_ENVIRONMENT.'.lck';
+if( is_readable($lock_file) ) die(1);
+
 require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+
