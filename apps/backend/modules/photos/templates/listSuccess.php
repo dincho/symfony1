e42b998f2fcd5577e929b5bc89a4c538feb26f71
vvidov <vvidov@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2010-09-26 06:11:06 +0000

    added private photos to backend/modules/photos/templates/listSuccess.php. added a new class 'private_photos_slot' to web/css/form_admin.css.
    (fixes #1994)

--- apps/backend/modules/photos/templates/listSuccess.php
+++ apps/backend/modules/photos/templates/listSuccess.php
@@ -34,6 +34,27 @@
                 <fieldset class="form_fields">
             <?php endif; ?>             
         <?php endforeach; ?>
+        <?php $photos = $member->getPrivateMemberPhotos(); $cnt_photos = count($photos); ?>
+        <?php $i=1; foreach($photos as $photo): ?>
+            <div class="private_photo_slot" id="<?php echo 'photo_' . $photo->getId(); ?>">
+                <div <?php if( $photo->isMain() ) echo 'class="selected_photo"'; ?>>
+                    <?php echo link_to(image_tag( ($photo->getImageFilename('cropped')) ? $photo->getImageUrlPath('cropped', '100x100') : $photo->getImageUrlPath('file', '100x100') ), 'members/editPhotos?id=' . $member->getId() . '&photo_id=' . $photo->getId()) ?><br />
+                </div>
+                
+                <?php echo link_to_remote('Delete Photo', array('url' => 'editProfile/confirmDeletePhoto?simple_delete=1&id=' . $photo->getId() . '&member_id=' . $member->getId(),
+                                                                         'update'  => 'msg_container',
+                                                                        )) ?><br />
+                                                                           
+                <?php echo link_to_unless($member->getPrivateDating(), 'Add to homepage', 'photos/addMemberPhotoToHomepage?photo_id=' . $photo->getId()); ?>
+
+                <?php include_partial('editProfile/photo_status', array('photo' => $photo)); ?>
+
+            </div>
+            <?php if( $i++ % 6 == 0 && $i <= $cnt_photos): ?>
+                </fieldset>
+                <fieldset class="form_fields">
+            <?php endif; ?>             
+        <?php endforeach; ?>
         </fieldset><br />
     <?php endforeach; ?>
     <?php include_partial('system/pager', array('pager' => $pager, 'route' => 'photos/list')); ?>
