13a33995a825a40efc0da8382cdb7cc03c0921fd
vvidov <vvidov@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2010-09-29 14:32:07 +0000

    Updated dropdown menu in _headerMenu.php
    (refs #1773)

--- apps/frontend/modules/content/templates/_headerMenu.php
+++ apps/frontend/modules/content/templates/_headerMenu.php
@@ -23,13 +23,33 @@
         <div id="dhtmlgoodies_contentBox">
           <div id="dhtmlgoodies_content">
             <ul class="top">
-                <li><strong><?php echo __('See profiles in your area:') ?></strong></li>
-                <li><?php echo link_to(__('Newly registered'), 'search/mostRecent', array('class' => 'sec_link', 'query_string' =>'filters[location]=1')) ?></li>
-                <li><?php echo link_to(__('Most recent visitors'), 'search/lastLogin', array('class' => 'sec_link', 'query_string' =>'filters[location]=1')) ?></li>
-                <li><?php echo link_to(__('Best matching you'), 'search/criteria', array('class' => 'sec_link', 'query_string' =>'filters[location]=1')) ?></li>
-                <li><?php echo link_to(__('You match them best'), 'search/reverse', array('class' => 'sec_link', 'query_string' =>'filters[location]=1')) ?></li>
-                <li><?php echo link_to(__('Best mutual matches'), 'search/matches', array('class' => 'sec_link', 'query_string' =>'filters[location]=1')) ?></li>
-                <li><?php echo link_to(__('Per your own rating'), 'search/byRate', array('class' => 'sec_link', 'query_string' =>'filters[location]=1')) ?></li>
+                <li><strong><?php echo __('Your Profile') ?></strong></li>
+                <li><?php echo link_to(__('Registration'), 'editProfile/registration', array('class' => 'sec_link')) ?> <span><?php echo __('(email, etc.)') ?></span></li>
+                <li><?php echo link_to(__('Self-description'), 'editProfile/selfDescription', array('class' => 'sec_link')) ?></li>
+                <li><?php echo link_to(__('Search Criteria (preferences)'), 'dashboard/searchCriteria', array('class' => 'sec_link')) ?></li>
+                <li><?php echo link_to(__('Posing/Essay'), 'editProfile/essay', array('class' => 'sec_link')) ?></li>
+                <li><?php echo link_to(__('Photos'), 'editProfile/photos', array('class' => 'sec_link')) ?></li>
+                  <?php if( !sfConfig::get('app_settings_imbra_disable') && $sf_user->getProfile()->getUsCitizen() == 1 ): ?>
+                      <li><?php echo link_to(__('IMBRA Information'), 'IMBRA/index', array('class' => 'sec_link')) ?></li>
+                  <?php endif; ?>
+                <?php if( in_array($sf_user->getProfile()->getMemberStatusId(), array(MemberStatusPeer::DEACTIVATED, MemberStatusPeer::DEACTIVATED_AUTO)) ): ?>
+                    <li><?php echo link_to(__('Activate profile (show)'), 'dashboard/deactivate', array('class' => 'sec_link')) ?></li>
+                <?php elseif( $sf_user->getProfile()->getMemberStatusId() == MemberStatusPeer::ACTIVE ): ?>
+                    <li><?php echo link_to(__('Deactivate profile (hide)'), 'dashboard/deactivate', array('class' => 'sec_link')) ?></li>
+                <?php endif; ?>
+            </ul>
+            <ul class="left">
+                <li><strong><?php echo __('Your Account') ?></strong></li>
+                <li><?php echo link_to(__('Email notifications'), 'dashboard/emailNotifications', array('class' => 'sec_link')) ?></li>
+                <li>
+                    <?php if( $sf_user->getProfile()->isFree() ): ?>
+                        <?php echo link_to(__('Subscribe'), 'subscription/index', array('class' => 'sec_link')) ?>
+                    <?php else: ?>
+                        <?php echo link_to(__('Manage Subscription'), 'subscription/manage', array('class' => 'sec_link')) ?>
+                    <?php endif; ?>
+                </li>
+                <li><?php echo link_to(__('Privacy'), 'dashboard/privacy', array('class' => 'sec_link')) ?></li>
+                <li><?php echo link_to(__('Delete your account'), 'dashboard/deleteYourAccount', array('class' => 'sec_link_brown')) ?></li>
             </ul>
             <script type="text/javascript">
             setSlideDownSpeed(4);
