d9bda45f9595614562984e644d041fcd251d025b
vvidov <vvidov@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2010-10-11 17:46:01 +0000

    changed content/templates/_timeout_warning.php so every page that has a 3 min timeout wanning to have a countdown minutes from 30 to 0. some minor changes to web/js/messagebar.js.
    (fixes #2047)

--- apps/frontend/modules/content/templates/_timeout_warning.php
+++ apps/frontend/modules/content/templates/_timeout_warning.php
@@ -1,16 +1,36 @@
 <?php use_helper('Javascript'); ?>
 
+
+<?php echo javascript_include_tag('messagebar') ?>
+
 <?php $minutes = sfConfig::get('app_settings_timeout_warning'); ?>
 <?php $warning = __('Your session will expire in %MINUTES% minutes - Please, click something to avoid being timed out.', array('%MINUTES%' => $minutes));?>
-<?php $timeout = (sfConfig::get('sf_timeout') - $minutes *60)*1000; ?>;
+<?php $timeout = (sfConfig::get('sf_timeout') - $minutes *60)*1000; ?>
 
 <script type="text/javascript" language="javascript">
 //<![CDATA[
-    Event.observe(window, 'load', function() {
-      setTimeout(function() {      
-        alert('<?php echo $warning; ?>');
-      }, <?php echo $timeout; ?>); 
+
+  var minuntes_left = <?php echo (sfConfig::get('sf_timeout'))/60; ?> +1;  
+  var warning = <?php echo "'".__('Please save this page within %TIMEOUT% minutes or your changes will be lost.')."'"; ?>  ;
   
+  function countDown()
+  {
+    minuntes_left--;
+    messagebar_message(warning.replace('%TIMEOUT%', minuntes_left.toString())); 
+  
+    TO = setTimeout( countDown, 1000 * 60 );
+    if(minuntes_left == 0)
+    {
+      clearTimeout(TO);
+    }
+  }
+  
+  Event.observe(window, 'load', function() {
+    setTimeout(function() {      
+      alert('<?php echo $warning; ?>');
+    }, <?php echo $timeout; ?>); 
+
+  countDown();
 });
 //]]>
 </script>
--- web/js/messagebar.js
+++ web/js/messagebar.js
@@ -1,10 +1,17 @@
-function messagebar_message(message)
+function messagebar_message(message, to_append)
 {
+  if (typeof to_append === "undefined") {
+    to_append = false;
+  }
+
   id = 'messageBar';
   var obj = document.getElementById(id);
   if(obj)
   {
-    obj.innerHTML = obj.innerHTML + '|&nbsp;&nbsp;&nbsp;'+ message+ '&nbsp;&nbsp;&nbsp;'
+    if(to_append)
+      obj.innerHTML = obj.innerHTML + '|&nbsp;&nbsp;&nbsp;'+ message+ '&nbsp;&nbsp;&nbsp;'
+    else
+      obj.innerHTML = '&nbsp;&nbsp;&nbsp;'+ message+ '&nbsp;&nbsp;&nbsp;'
   }
   else
   {
