90a2ba7278ea2071f370ef5c01f91d6d95e0f822
vvidov <vvidov@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2010-11-04 19:15:10 +0000

    coplete refactiring od save_changes.js. tested all form on IE6/7/8, ff3, chrome, safary5
    (fixes #1652)

--- web/js/save_changes.js
+++ web/js/save_changes.js
@@ -1,75 +1,58 @@
 // JavaScript Document
 var hash = new Array();
-var ch_hash = new Array();
 
 
-function checked_item(id, checked)
+function checked_item(id, type, value, checked)
 {
   this.id;
+  this.type;
+  this.value;
   this.checked;
   
-  this.GetId = function() { return id;}
-  this.GetChecked = function() { return checked;}
+  this.getId = function() { return id;}
+  this.getType = function() { return type;}
+  this.getValue = function() { return value;}
+  this.getChecked = function() { return checked;}
 }
 
 
-function enable_save_btn()
-{
-   if($("save_btn").disabled)
-   {
-    $("save_btn").enable();
-   }
-}
 
 function validate_save_btn()
 {
   for(var i=0; i< hash.length; i++)
   {
-    if($(hash[i].id).value != hash[i].value )
+    if(hash[i].getType() == 'checkbox' || hash[i].getType() == 'radio')
     {
-      $("save_btn").enable();
-      return; 
-    } 
+      if($(hash[i].getId().toString()).checked != hash[i].getChecked() )
+      {
+        $("save_btn").enable();
+        return; 
+      }
+    }
+    else
+    {
+      if($(hash[i].getId().toString()).style.display !='none' && $(hash[i].getId().toString()).value != hash[i].getValue() )
+      {
+        $("save_btn").enable();
+        return;
+      } 
+    }
   }
-  for(var i=0; i< ch_hash.length; i++)
-  {
-    if($(ch_hash[i].GetId().toString()).checked != ch_hash[i].GetChecked() )
-    {
-      $("save_btn").enable();
-      return;
-    } 
-  }  
-  $("save_btn").disable(); 
+  $("save_btn").disable();
 }
+
 
 Event.observe(window, 'load',function()
 {
+  $("save_btn").disable(); 
   var fields = $$("input", "textarea", "select");
   for (var i = 0; i < fields.length; i++) 
   {
-    if(fields[i].type != 'submit' && fields[i].type != 'button' &&
-       fields[i].type != 'checkbox' && fields[i].type != 'radio' &&
-       fields[i].type != 'hidden' )
-    {
-      hash.push(Object.clone(fields[i]));
-    
-      fields[i].observe('keypress', enable_save_btn);
-      fields[i].observe('blur', validate_save_btn);
-      fields[i].observe('change', validate_save_btn);
-      
-      
-    }
-    else if(fields[i].type == 'checkbox' || fields[i].type == 'radio')
-    {
-      ch_hash.push( new checked_item([fields[i].id], fields[i].checked));  
+      hash.push( new checked_item([fields[i].id], fields[i].type, fields[i].value, fields[i].checked));  
 
       fields[i].observe('keypress', validate_save_btn);
       fields[i].observe('click', validate_save_btn);
       fields[i].observe('blur', validate_save_btn);
       fields[i].observe('change', validate_save_btn);
-    }  
   }
-  
-  $("save_btn").disable(); 
-
 });
