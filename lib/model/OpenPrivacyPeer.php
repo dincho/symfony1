32e1298a26b25c50ed4e1e70e2a7a485463a5ff0
vvidov <vvidov@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2011-01-09 09:28:14 +0000

    a small correction of getPrivacy in OpenPrivacyPeer.php
    (refs #2126)

--- lib/model/OpenPrivacyPeer.php
+++ lib/model/OpenPrivacyPeer.php
@@ -15,7 +15,8 @@ class OpenPrivacyPeer extends BaseOpenPrivacyPeer
 		$c = new Criteria();
 		$c->add(OpenPrivacyPeer::MEMBER_ID, $member_id);
 		$c->add(OpenPrivacyPeer::PROFILE_ID, $profile_id);
+ 		$v = OpenPrivacyPeer::doSelect($c);
 
-    return (bool) OpenPrivacyPeer::doCount($c);
+		return !empty($v) ? $v[0] : null;
 	}
 }
