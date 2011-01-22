ce1103ec9b4f1fc375d50405107d9212a6b88fc7
vvidov <vvidov@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2011-01-22 14:48:37 +0000

    do not show an alert on page photos.html for Mac OS X and ff3 or safari.
    (fixes #2150)

--- apps/frontend/modules/content/templates/_timeout_warning.php
+++ apps/frontend/modules/content/templates/_timeout_warning.php
@@ -11,6 +11,14 @@
 //<![CDATA[
   
   Event.observe(window, 'load', function() {
+      // do not show an alert on page 'photos.html' in case of Mac OS X and ff3 or safari      
+     if( (/Firefox/.test( navigator.userAgent ) || (/Safari/.test( navigator.userAgent ) && !/Chrome/.test( navigator.userAgent )))&& 
+        /Mac OS X/.test( navigator.userAgent ) &&
+        /photos.html/.test( window.location.href ))
+     {
+        return;
+     }    
+    
     setTimeout(function() {      
       alert('<?php echo $warning; ?>');
     }, <?php echo $timeout; ?>) 
