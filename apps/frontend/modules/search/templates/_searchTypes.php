74aefdf7a63a0fc32be68a75581a97935ee22fd6
vvidov <vvidov@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2011-01-31 19:00:37 +0000

    fixed search type NOT to allow to click on search type that is 'selected'.
    (fixes #2177)

--- apps/frontend/modules/search/templates/_searchTypes.php
+++ apps/frontend/modules/search/templates/_searchTypes.php
@@ -1,12 +1,13 @@
 <?php $action_name = sfContext::getInstance()->getActionName(); ?>
 <div id="search_types">
-<?php $options = array('query_string' =>'filters[location]=0', 'class' => 'sec_link', 'onclick' => "show_loader('match_results','".__('Updating Results...')."')"); ?>
+<?php $options = array('query_string' =>'filters[location]=0', 'class' => 'sec_link'); ?>
+<?php $search_options = array('query_string' =>'filters[location]=0', 'class' => 'sec_link', 'onclick' => "show_loader('match_results','".__('Updating Results...')."')"); ?>
 <?php echo link_to_unless($action_name == 'mostRecent', __('Most Recent'), 'search/mostRecent', $options ) ?>
-&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'lastLogin', __('Last Login'), 'search/lastLogin', $options ) ?>
-&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'criteria', __('Custom (by Criteria)'), 'search/criteria', $options ) ?>
-&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'reverse', __('Reverse'), 'search/reverse', $options ) ?>
-&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'matches', __('Matches'), 'search/matches', $options ) ?>
-&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'byKeyword', __('by Keyword'), 'search/byKeyword', $options ) ?>
-&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'profileID', __('Profile ID'), 'search/profileID', $options ) ?>
-&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'byRate', __('by Rate'), 'search/byRate', $options ) ?>
+&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'lastLogin', __('Last Login'), 'search/lastLogin', $action_name == 'lastLogin' ? $options : $search_options ) ?>
+&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'criteria', __('Custom (by Criteria)'), 'search/criteria', $action_name == 'criteria' ? $options : $search_options ) ?>
+&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'reverse', __('Reverse'), 'search/reverse', $action_name == 'reverse' ? $options : $search_options ) ?>
+&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'matches', __('Matches'), 'search/matches', $action_name == 'matches' ? $options : $search_options ) ?>
+&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'byKeyword', __('by Keyword'), 'search/byKeyword', $action_name == 'byKeyword' ? $options : $search_options ) ?>
+&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'profileID', __('Profile ID'), 'search/profileID', $action_name == 'profileID' ? $options : $search_options ) ?>
+&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<?php echo link_to_unless($action_name == 'byRate', __('by Rate'), 'search/byRate', $action_name == 'byRate' ? $options : $search_options ) ?>
 </div>
