548f91049aef0824afa5d7f744adac9fceafb505
vvidov <vvidov@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2011-03-05 15:27:23 +0000

    fixed photoAccessSuccess.php template to use same styles and div like winks page.
    (fixes #2207)

--- apps/frontend/modules/dashboard/templates/photoAccessSuccess.php
+++ apps/frontend/modules/dashboard/templates/photoAccessSuccess.php
@@ -8,7 +8,7 @@
             <?php $member = $perm->getMemberRelatedByMemberId(); ?>
             <div class="member_profile">
                 <h2><?php echo Tools::truncate($member->getEssayHeadline(), 40) ?></h2> <span class="number"><?php echo $member->getAge() ?></span>
-                <?php echo link_to_ref(profile_photo($member, 'float-left'), '@profile?bc=hotlist&username=' . $member->getUsername()) ?>
+                <?php echo link_to_ref(profile_photo($member), '@profile?bc=hotlist&username=' . $member->getUsername(), array('class' => 'photo_link', )) ?>
                 <div class="input">
                     <span class="public_reg_notice">
                         <?php echo __('%she_he% granted you access %date%', 
@@ -28,8 +28,9 @@
         <span><?php echo __('Click on the "x" in the lower corner of a profile to remove it from the list.')?></span><br /><br />
         <?php foreach ($my_grants as $perm): ?>
             <?php $profile = $perm->getMemberRelatedByProfileId(); ?>
-            <div class="privacy_profile_viewers" id="member_profile_<?php echo $profile->getId();?>">
+            <div class="member_profile" id="member_profile_<?php echo $profile->getId();?>">
                 <h2><?php echo Tools::truncate($profile->getEssayHeadline(), 40) ?></h2><span class="number"><?php echo $profile->getAge() ?></span>
+                <?php echo link_to_ref(profile_photo($profile), '@profile?bc=photoAccess&username=' . $profile->getUsername(), array('class' => 'photo_link', )) ?>
                 <div class="input">
                     <span class="public_reg_notice"><?php echo __('Access granted %date%', array('%date%' => distance_of_time_in_words($perm->getCreatedAt(null)))) ?></span>
                     <?php echo link_to_ref(__('View Profile'), '@profile?bc=photoAccess&username=' . $profile->getUsername(), array('class' => 'sec_link')) ?><br />
@@ -39,14 +40,13 @@
                                                     'script' => true, 
                                                     'after' => '$("member_profile_'.$profile->getId().'").remove();'
                                             ), array('class' => 'sec_link', )); ?>
-                    <?php echo link_to_remote(image_tag('butt_x.gif', 'class=x'), array(
-                                                    'url' => '@toggle_private_photos_perm?username=' . $profile->getUsername(),
-                                                    'update' => array('success' => 'msg_container'),
-                                                    'script' => true, 
-                                                    'after' => '$("member_profile_'.$profile->getId().'").remove();'
-                                            ), array('class' => 'sec_link', )); ?>
                 </div>
-                <?php echo link_to_ref(profile_photo($profile), '@profile?bc=photoAccess&username=' . $profile->getUsername()) ?>
+                  <?php echo link_to_remote(image_tag('butt_x.gif', 'class=x'), array(
+                                                  'url' => '@toggle_private_photos_perm?username=' . $profile->getUsername(),
+                                                  'update' => array('success' => 'msg_container'),
+                                                  'script' => true, 
+                                                  'after' => '$("member_profile_'.$profile->getId().'").remove();'
+                                          ), array('class' => 'sec_link', )); ?>
             </div>        
         <?php endforeach; ?>
     </div>
