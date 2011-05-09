bc093d76d2e3e4640a1e790bd5a10e081f7a34e8
vvidov <vvidov@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2011-05-09 14:48:28 +0000

    added 'notice' in messages/templates/sendSuccess.php template. added a class for 'notice' in css/main.css.
    (fixes #2250)

--- apps/frontend/modules/messages/templates/sendSuccess.php
+++ apps/frontend/modules/messages/templates/sendSuccess.php
@@ -42,6 +42,9 @@
     </fieldset>
 
     <fieldset class="background_f4">
+        <div id="send_text" class="new_message_notice"><span><?php echo __('Never include your last name, e-mail address, home address, phone number, 
+              place of work and any other identifying information in initial messages with other members'); ?></span>
+        </div>
         <?php echo pr_label_for('content', __('Message:')) ?>
         <?php echo textarea_tag('content',  $draft->getBody(), array('id' => 'your_story', 
                                                                         'rows' => 10, 'cols' => 30)) ?>
@@ -68,6 +71,7 @@
                                                    array('class' => 'button', )) ?>
         <br />
     </fieldset>
+    <br />
   
 </form>
 
