d782045adc599810c90cbeb74fa77c0e5696b6c0
dincho <dincho@xbuv.com>
2011-05-17 20:42:00 +0000

    more changes because of the new test server

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -1,7 +1,7 @@
 <?php
 $paypal_addreses = array('216.113.188.202', '216.113.188.203', '216.113.188.204', '66.211.170.66', '216.113.191.33');
 
-if (!in_array(@$_SERVER['REMOTE_ADDR'], array_merge($paypal_addreses, array('127.0.0.1', '::1', '212.36.28.66', '10.0.0.7'))))
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array_merge($paypal_addreses, array('127.0.0.1', '::1', '212.36.28.66', '10.0.0.7', '78.83.16.68'))))
 {
   header('HTTP/1.1 403 Forbidden');
   die('Access denied.');
