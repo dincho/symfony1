612f872ab5919a0ecff0e71eee41924359e98e2a
vvidov <vvidov@2f6e7929-e1a3-4d0b-8d75-4d86a92f4f2c>
2011-05-29 17:48:00 +0000

    added one more dir to clearCache in StaticPage.php.
    (fixes #2257)

--- lib/model/StaticPage.php
+++ lib/model/StaticPage.php
@@ -14,5 +14,7 @@ class StaticPage extends BaseStaticPage
       $sf_root_cache_dir = sfConfig::get('sf_root_cache_dir');
       $cache = $sf_root_cache_dir.'/frontend/*/template/*/all/*/content/page/slug/' . $this->getSlug() . '/*';
       sfToolkit::clearGlob($cache);
+      $cache = $sf_root_cache_dir.'/frontend/*/template/*/all/*/page/' . $this->getSlug() . '.*';
+      sfToolkit::clearGlob($cache);
     }
 }
