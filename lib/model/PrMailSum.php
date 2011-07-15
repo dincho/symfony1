7bd540abf6d37b525cefc727f5aaf573618aa0f6
dincho <dincho@xbuv.com>
2011-07-15 06:24:52 +0000

    added missing PrMailSum model files

---
+++ lib/model/PrMailSum.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'pr_mail_sum' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class PrMailSum extends BasePrMailSum
+{
+}
---
+++ lib/model/PrMailSumPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'pr_mail_sum' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class PrMailSumPeer extends BasePrMailSumPeer
+{
+}
