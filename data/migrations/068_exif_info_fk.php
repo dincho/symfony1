0aa59492b9afc5c6d7971b02525b772f53937050
dincho <dincho@xbuv.com>
2011-11-07 12:38:04 +0000

    missing ON DELETE option of the FK ( refs #2417 )

--- data/migrations/068_exif_info_fk.php
+++ data/migrations/068_exif_info_fk.php
@@ -11,7 +11,7 @@ class Migration068 extends sfMigration
     public function up()
     {
         $this->executeSQL("DELETE FROM `photo_exif_info` WHERE `photo_id` NOT IN (SELECT DISTINCT p.id FROM `member_photo` p)");
-        $this->executeSQL("ALTER TABLE `photo_exif_info` ADD CONSTRAINT `photo_exif_info_fk` FOREIGN KEY (`photo_id`) REFERENCES `member_photo` (`id`)");
+        $this->executeSQL("ALTER TABLE `photo_exif_info` ADD CONSTRAINT `photo_exif_info_fk` FOREIGN KEY (`photo_id`) REFERENCES `member_photo` (`id`) ON DELETE CASCADE");
     }
 
     /**
