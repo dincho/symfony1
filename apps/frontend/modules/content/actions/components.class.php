51d3e0da51394eb07e567bb6c69d1bb00b841096
dincho <dincho@xbuv.com>
2011-12-22 18:31:38 +0000

    cleanup ( refs #2502 @0h30m )

--- apps/frontend/modules/content/actions/components.class.php
+++ apps/frontend/modules/content/actions/components.class.php
@@ -10,50 +10,7 @@ class contentComponents extends sfComponents
     {
         
     }
-    
-    public function executeHomepagePhotoSet()
-    {
-        $catalog_id = $this->getUser()->getCatalogId();
-        
-        $c = new Criteria();
-        $c->add(StockPhotoPeer::HOMEPAGES, null, Criteria::ISNOTNULL);
-        $c->add(StockPhotoPeer::HOMEPAGES, 'FIND_IN_SET("' . $catalog_id .'", ' . StockPhotoPeer::HOMEPAGES . ') != 0', Criteria::CUSTOM);
-        $c->add(StockPhotoPeer::HOMEPAGES_SET, $this->homepage_set);
-        $c->addAscendingOrderByColumn(StockPhotoPeer::HOMEPAGES_POS);
-        $c->setLimit(9);
-        $photos = StockPhotoPeer::doSelect($c);
-        
-        if( count($photos) < 9 && $this->homepage_set != 1)
-        {
-            $c->add(StockPhotoPeer::HOMEPAGES_SET, 1);
-            $photos = StockPhotoPeer::doSelect($c);
-        }
-        
-        $this->photos = ( count($photos) == 9) ? $photos : array();
-    }
-    
-    public function executeHomepageMemberPhotoSet()
-    {
-        $catalog_id = $this->getUser()->getCatalogId();
-        
-        $c = new Criteria();
-        $c->add(HomepageMemberPhotoPeer::HOMEPAGES, null, Criteria::ISNOTNULL);
-        $c->add(HomepageMemberPhotoPeer::HOMEPAGES, 'FIND_IN_SET("' . $catalog_id .'", ' . HomepageMemberPhotoPeer::HOMEPAGES . ') != 0', Criteria::CUSTOM);
-        $c->add(HomepageMemberPhotoPeer::HOMEPAGES_SET, $this->homepage_set);
-        $c->addGroupByColumn(HomepageMemberPhotoPeer::HOMEPAGES_POS);
-        //$c->addAscendingOrderByColumn(HomepageMemberPhotoPeer::HOMEPAGES_POS);
-        $c->setLimit(9);
-        $photos = HomepageMemberPhotoPeer::doSelect($c);
-        
-        if( count($photos) < 9 && $this->homepage_set != 1)
-        {
-            $c->add(HomepageMemberPhotoPeer::HOMEPAGES_SET, 1);
-            $photos = HomepageMemberPhotoPeer::doSelect($c);
-        }
-        
-        $this->photos = ( count($photos) == 9) ? $photos : array();
-    }
-    
+
     public function executeHomepageSinglePhoto()
     {
         $catalog_id = $this->getUser()->getCatalogId();
--- lib/model/StockPhoto.php
+++ lib/model/StockPhoto.php
@@ -48,58 +48,7 @@ class StockPhoto extends BaseStockPhoto
         $this->setCropped($newName);
         $this->save();
         $this->createThumbnails('cropped');
-        
-        $this->addEffects('cropped', '308x293');
     }
-    
-    public function addEffects($field, $size)
-    {
-        $originalPath = $this->getImagePath($field, $size);
-        if( $this->getGender() == 'M')
-        {
-            $newName = ($field != 'file') ? 'blue_crop_' . $this->getFile() : 'blue_' . $this->getFile();
-            $hue = 195; $saturation = 25;
-        } elseif($this->getGender() == 'F')
-        {
-            $newName = ($field != 'file') ? 'orange_crop_' . $this->getFile(): 'orange_' . $this->getFile();
-            $hue = 25; $saturation = 27;
-        } else {
-            throw new sfException('StockPhoto::tint No gender set!');
-        }
-        
-        
-        $newPath = $this->getImagesPath() . $size . DIRECTORY_SEPARATOR . $newName;
-        $cropped = new sfThumbnail();
-        $cropped->loadFile($originalPath);
-        $cropped->addPrEffects($hue, $saturation);
-        $cropped->save($newPath);
-    }
-    
-    public function getTiltImageUrlPath($size = '308x293')
-    {
-        $prefix = ($this->getGender() == 'F') ? 'orange_' : 'blue_'; 
-        
-        $filename = ($this->getImageFilename('cropped')) ? $this->getImageFilename('cropped') : $this->getImageFilename('file');
-        return _compute_public_path($prefix . $filename, 'uploads/images/' . $this->getImagesDir() . $size, null, false);
-    }
-    
-    public function delete($con = null)
-    {
-        if( $this->getGender() == 'M')
-        {
-            $name = 'blue_crop_' . $this->getFile();
-            $name2 = 'blue_' . $this->getFile();
-        } elseif($this->getGender() == 'F')
-        {
-            $name = 'orange_crop_' . $this->getFile();
-            $name2 = 'orange_' . $this->getFile();
-        }
-                
-        @unlink($this->getImagesPath() . '100x95' . DIRECTORY_SEPARATOR . $name);
-        @unlink($this->getImagesPath() . '100x95' . DIRECTORY_SEPARATOR . $name2);
-        parent::delete($con);
-    }
-
 }
 
 $sizes = array(array('width' => 50, 'height' => 50),
