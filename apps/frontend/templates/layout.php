86d427e1fdc61e876f37f95c4d5f57bf5807a666
dincho <dincho@me.com>
2013-07-18 12:05:22 +0300

    google remarking integration (closes #257)

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -96,5 +96,19 @@
       js.src = "//connect.facebook.net/<?php echo ($sf_user->getCulture() == 'pl') ? 'pl_PL' : 'en_US'; ?>/all.js#xfbml=1";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
+    <!-- Google Code for Remarketing Tag -->
+    <script type="text/javascript">
+      /* <![CDATA[ */
+      var google_conversion_id = 1064048011;
+      var google_custom_params = window.google_tag_params;
+      var google_remarketing_only = true;
+      /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+    <noscript>
+      <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1064048011/?value=0&amp;guid=ON&amp;script=0"/>
+      </div>
+    </noscript>
 </body>
 </html>
