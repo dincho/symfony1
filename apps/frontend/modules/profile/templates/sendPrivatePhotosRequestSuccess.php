9ee579f8962a571c8bf420dc1653d775111b3e38
venimus <venimus@gmail.com>
2013-10-03 15:02:13 +0300

    Remove ";" from profile page

--- apps/frontend/modules/profile/templates/sendPrivatePhotosRequestSuccess.php
+++ apps/frontend/modules/profile/templates/sendPrivatePhotosRequestSuccess.php
@@ -1,5 +1,4 @@
 <?php use_helper('Javascript') ?>
 <?php include_partial('content/messages'); ?>
 <?php $title_req = __('You already requested access to %USERNAME%\'s private photos.', array('%USERNAME%' => $profile->getUsername())); ?>
-<?php echo javascript_tag('$("private_photos").update("'.$title_req.'");' ); ?>;
-
+<?php echo javascript_tag('$("private_photos").update("'.$title_req.'");' ); ?>
