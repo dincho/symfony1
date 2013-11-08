7bf2dc848b909ab7fbaf3726e4fabbffd71f49f0
venimus <venimus@gmail.com>
2013-11-08 17:28:27 +0200

    move TOS checkox to the right

--- apps/frontend/modules/content/templates/indexSuccess.php
+++ apps/frontend/modules/content/templates/indexSuccess.php
@@ -46,8 +46,8 @@
                         
                         <?php $tos_text = __('Homepage - I am 18 or older and I agree to the <a href="%URL_FOR_TERMS%" class="sec_link">Terms of Use</a> and <a href="%URL_FOR_PRIVACY_POLICY%" class="sec_link">Privacy Policy</a>.') ?>
                         <div class="tos">
-                            <?php echo checkbox_tag('tos', 1, false); ?>
                             <?php echo content_tag('label', $tos_text); ?>
+                            <?php echo checkbox_tag('tos', 1, false); ?>
                         </div>
                         
                     </fieldset>
