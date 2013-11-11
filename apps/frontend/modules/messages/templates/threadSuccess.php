035b9313c99f3ddf206cd7a963e396bde26a704f
venimus <venimus@gmail.com>
2013-11-11 19:52:58 +0200

    fix html

--- apps/frontend/modules/messages/templates/threadSuccess.php
+++ apps/frontend/modules/messages/templates/threadSuccess.php
@@ -63,7 +63,8 @@
                         <?php echo __('Internal System'); ?><br />
                     <?php endif; ?>
                     <?php echo format_date_pr($message->getCreatedAt(null), null, 'dd-MMM-yyyy', $member->getTimezone()); ?>
-                <td  class="message_body">
+                </td>
+                <td class="message_body">
                     <?php echo strip_tags($message->getBody(ESC_RAW), '<br><a>'); ?>
                 </td>
             </tr>
@@ -99,7 +100,7 @@
     
         <fieldset class="background_000">
             <label></label>
-            <?php echo submit_tag(__('Send'), array('class' => 'button', 'onClick' => "if(submitted) return false; messagebar_message('".__('Sending message...')."'); submitted = true; return true;") ) ?>
+            <?php echo submit_tag(__('Send'), array('class' => 'button', 'onclick' => "if(submitted) return false; messagebar_message('".__('Sending message...')."'); submitted = true; return true;") ) ?>
             <?php echo button_to_function(__('Save Now'), 'save_draft();', array('class' => 'button', 'id' => 'save_to_draft_btn')) ?>
             <?php echo button_to(__('Discard'), 'messages/discard?draft_id=' . $draft->getId(), array('class' => 'button', )) ?>
             <br />
