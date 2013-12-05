fb7a1d0c0865fc6fb891c48b69da0a5802e8f3d5
dincho <dincho@users.noreply.github.com>
2013-12-05 13:32:39 +0200

    removed qbaka because it need environment specific implementation

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -7,9 +7,6 @@
 
 <?php include_title() ?>
 <?php require_once(dirname(__FILE__).'/_favicon.inc.php'); ?>
-<script type="text/javascript">
-window.qbaka || (function(a,c){a.__qbaka_eh=a.onerror;a.__qbaka_reports=[];a.onerror=function(){a.__qbaka_reports.push(arguments);if(a.__qbaka_eh)try{a.__qbaka_eh.apply(a,arguments)}catch(b){}};a.onerror.qbaka=1;a.qbaka={report:function(){a.__qbaka_reports.push([arguments, new Error()]);},customParams:{},set:function(a,b){qbaka.customParams[a]=b},exec:function(a){try{a()}catch(b){qbaka.reportException(b)}},reportException:function(){}};var b=c.createElement("script"),e=c.getElementsByTagName("script")[0],d=function(){e.parentNode.insertBefore(b,e)};b.type="text/javascript";b.async=!0;b.src="//cdn.qbaka.net/reporting.js";"[object Opera]"==a.opera?c.addEventListener("DOMContentLoaded",d):d();qbaka.key="9a1f53194e01847189fe2ddd58d51fd2"})(window,document);qbaka.options={autoStacktrace:1,trackEvents:1};
-</script>
 </head>
 
 <body>
