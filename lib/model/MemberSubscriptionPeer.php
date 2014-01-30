f2b188b548cccbd9c8075046c7783a8a4b5e716d
dincho <dincho@users.noreply.github.com>
2014-01-30 12:32:38 +0200

    subscription/payment re-factored and formatting

--- lib/model/MemberSubscriptionPeer.php
+++ lib/model/MemberSubscriptionPeer.php
@@ -9,10 +9,34 @@
  */ 
 class MemberSubscriptionPeer extends BaseMemberSubscriptionPeer
 {
-  public static function retrieveByPPRef($ref)
-  {
-    $c = new Criteria();
-    $c->add(self::PP_REF, $ref);
-    return self::doSelectOne($c);
-  }  
+    public static function retrieveByPPRef($ref)
+    {
+        $c = new Criteria();
+        $c->add(self::PP_REF, $ref);
+        return self::doSelectOne($c);
+    }  
+
+    public static function getOrCreatePendingSubscription(
+        Member $member,
+        SubscriptionDetails $subscription
+    ) {
+        //check if pending subscription already exists
+        $c = new Criteria();
+        $c->add(MemberSubscriptionPeer::MEMBER_ID, $member->getId());
+        $c->add(MemberSubscriptionPeer::SUBSCRIPTION_ID, $subscription->getSubscriptionId());
+        $c->add(MemberSubscriptionPeer::STATUS, 'pending');
+        $memberSubscription = MemberSubscriptionPeer::doSelectOne($c);
+
+        //no subscription found, create new one.
+        if (!$memberSubscription) {
+          $memberSubscription = new MemberSubscription();
+          $memberSubscription->setMemberId($member->getId());
+          $memberSubscription->setSubscriptionId($subscription->getSubscriptionId());
+          $memberSubscription->setPeriod($subscription->getPeriod());
+          $memberSubscription->setPeriodType($subscription->getPeriodType());
+          $memberSubscription->save();
+        }
+
+        return $memberSubscription;
+    }
 }
