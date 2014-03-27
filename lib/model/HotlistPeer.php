ce0901f5bbc66bc2985a7b3062c99e0d6c195334
dincho <dincho@users.noreply.github.com>
2014-03-27 19:22:33 +0200

    formatting & cleanup

--- lib/model/HotlistPeer.php
+++ lib/model/HotlistPeer.php
@@ -9,34 +9,19 @@
  */ 
 class HotlistPeer extends BaseHotlistPeer
 {
-  public static function getNewHotlistCriteria($member_id)
-  {
+    public static function getNewHotlistCriteria($member_id)
+    {
         $c = new Criteria();
         $c->add(HotlistPeer::PROFILE_ID, $member_id);
         $c->add(HotlistPeer::IS_NEW, true);
         $c->addJoin(MemberPeer::ID, HotlistPeer::MEMBER_ID);
         $c->add(MemberPeer::MEMBER_STATUS_ID, MemberStatusPeer::ACTIVE); //don not show unavailable profiles
-        
-        //privacy check
-        $c->addJoin(HotlistPeer::MEMBER_ID, OpenPrivacyPeer::MEMBER_ID.' AND '. HotlistPeer::PROFILE_ID .' = '. OpenPrivacyPeer::PROFILE_ID, Criteria::LEFT_JOIN);
-        $open_privacy_check = sprintf("IF(%s = 1 AND %s IS NULL, FALSE, TRUE) = TRUE", MemberPeer::PRIVATE_DATING, OpenPrivacyPeer::ID);
-        $c->add(OpenPrivacyPeer::ID, $open_privacy_check, Criteria::CUSTOM);
-        
-        return $c;
-  }
 
-  public static function getAllHotlistCriteria($member_id)
-  {
-        $c = new Criteria();
-        $c->add(HotlistPeer::PROFILE_ID, $member_id);
-        $c->addJoin(MemberPeer::ID, HotlistPeer::MEMBER_ID);
-        $c->add(MemberPeer::MEMBER_STATUS_ID, MemberStatusPeer::ACTIVE); //don not show unavailable profiles
-        
         //privacy check
         $c->addJoin(HotlistPeer::MEMBER_ID, OpenPrivacyPeer::MEMBER_ID.' AND '. HotlistPeer::PROFILE_ID .' = '. OpenPrivacyPeer::PROFILE_ID, Criteria::LEFT_JOIN);
         $open_privacy_check = sprintf("IF(%s = 1 AND %s IS NULL, FALSE, TRUE) = TRUE", MemberPeer::PRIVATE_DATING, OpenPrivacyPeer::ID);
         $c->add(OpenPrivacyPeer::ID, $open_privacy_check, Criteria::CUSTOM);
-        
+
         return $c;
-  }
+    }
 }
