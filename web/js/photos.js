0178cba8f7545c87da20b649a8f97ac5e9747d39
dincho <dincho@users.noreply.github.com>
2014-03-31 13:45:32 +0300

    removed message function from photos.js

--- web/js/photos.js
+++ web/js/photos.js
@@ -1,34 +1,5 @@
 jQuery.noConflict();
 
-function clearMessages() {
-    if ($('msg_container')) {
-        $('msg_container').innerHTML = '';
-    }
-}
-
-// add message in static container
-function addMessage(message, divClass) {
-    divClass = divClass === undefined ? 'msg' : divClass;
-
-    var $msgContainer = $('msg_container');
-    var $msgs = $('msgs');
-
-    if (!$msgContainer) {
-        return;
-    }
-
-    if (!$msgs) {
-        $msgs = $(document.createElement('div'));
-        $msgs.setAttribute('id', 'msgs');
-        $msgContainer.appendChild($msgs);
-    }
-
-    message = '<div class="' + divClass + '">' + message + '</div>';
-    message = $msgs.innerHTML + message;
-
-    $msgs.innerHTML = message;
-}
-
 function moveElement(draggable, droparea, event)
 {
     var draggable_parent = draggable.parentNode;
