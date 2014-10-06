cde1102cc5a96afb9dd6442679b8e76bf6c87a76
Julian Atanasov <julianatanasov@live.com>
2014-10-06 14:18:49 +0300

    admin-conversations: typo

--- data/migrations/005_admin_messages_to_single_thread.php
+++ data/migrations/005_admin_messages_to_single_thread.php
@@ -3,7 +3,7 @@
 class Migration005
 {
     /**
-     * Migrate the schema up, from the version 003 to the current one.
+     * Migrate the schema up, from the version 004 to the current one.
      */
     public function up()
     {
@@ -25,7 +25,7 @@ class Migration005
     }
 
     /**
-     * Migrate the schema down to version 003, i.e. undo the modifications made in up()
+     * Migrate the schema down to version 004, i.e. undo the modifications made in up()
      */
     public function down()
     {
