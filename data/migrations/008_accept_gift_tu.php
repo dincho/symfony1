ac33d5d8fce333bc3c918f321808084979262e7d
venimus <venimus@users.noreply.github.com>
2015-03-16 15:24:35 +0200

    Accept Gift action

---
+++ data/migrations/008_accept_gift_tu.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Migrations between versions 007 and 008
+ */
+class Migration008 extends sfMigration
+{
+    /**
+    * Migrate up to version 008
+    */
+    public function up()
+    {
+        $this->loadTransUnits();
+    }
+
+    /**
+    * Migrate down to version 007
+    */
+    public function down()
+    {
+        $this->deleteTransUnits();
+    }
+}
---
+++ lib/model/Gift.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'gift' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class Gift extends BaseGift
+{
+    public function getExpiresAt()
+    {
+        $giftExpires = new DateTime($this->getCreatedAt());
+
+        return $giftExpires->add(new DateInterval('P' . sfConfig::get('gifts_expire_days') . 'D'));
+    }
+
+    public function isExpired()
+    {
+        return $this->getAccepted() || $this->getExpiresAt() < new DateTime();
+    }
+}
