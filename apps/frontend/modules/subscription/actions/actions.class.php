1b3d2c8c0b1e43ca6579e82eeae4ce8ea0bcd0a5
Julian Atanasov <julianatanasov@live.com>
2015-03-17 17:32:53 +0200

    refactor

--- apps/frontend/modules/subscription/actions/actions.class.php
+++ apps/frontend/modules/subscription/actions/actions.class.php
@@ -226,7 +226,7 @@ class subscriptionActions extends prActions
                 return false;
             }
 
-            if (GiftPeer::giftAlreadyPending($this->member, $email)) {
+            if (GiftPeer::canSendGiftToEmail($this->member, $email)) {
                 $this->setFlash('msg_error', 'You have already sent a gift to this email address');
                 return false;
             }
--- lib/model/GiftPeer.php
+++ lib/model/GiftPeer.php
@@ -11,24 +11,11 @@ class GiftPeer extends BaseGiftPeer
 {
     public static function getAllowedGiftsNum(BaseMember $member)
     {
-        // calculate last member payment
-        $member_subscription = $member->getCurrentMemberSubscription();
-        $last_payment = ($member_subscription) ? $member_subscription->getLastCompletedPayment() : null;
-
-        if (!$last_payment) {
+        if (!$member->getLastCompletedPayment()) {
             return 0;
         }
 
-        $paymentDate = $last_payment->getCreatedAt(null); //timestamp
-        $gracePeriod = sfConfig::get('app_gifts_send_period_days', 7)*24*60*60;  // 604800 secs
-        $endDate = $paymentDate + $gracePeriod;
-
-        $c = new Criteria();
-        $c->add(GiftPeer::FROM_MEMBER_ID, $member->getId());
-        $c->add(GiftPeer::CREATED_AT, $paymentDate, Criteria::GREATER_THAN);
-        $c->add(GiftPeer::CREATED_AT, $endDate, Criteria::LESS_THAN);
-        $giftsCnt = GiftPeer::doCount($c);
-
+        $giftsCnt = self::countMemberSentGifts($member, new Criteria());
         $giftsLimit = sfConfig::get('app_gifts_friends_limit', 2);
 
         return $giftsLimit - $giftsCnt;
@@ -45,27 +32,31 @@ class GiftPeer extends BaseGiftPeer
         return self::doSelectOne($c);
     }
 
-    public static function giftAlreadyPending(BaseMember $member, $email)
+    public static function canSendGiftToEmail(BaseMember $member, $email)
     {
-        // calculate last member payment
-        $member_subscription = $member->getCurrentMemberSubscription();
-        $last_payment = ($member_subscription) ? $member_subscription->getLastCompletedPayment() : null;
+        $c = new Criteria();
+        $c->add(GiftPeer::TO_EMAIL, $email);
+        $giftsCnt = self::countMemberSentGifts($member, $c);
+
+        return $giftsCnt > 0;
+    }
+
+    public static function countMemberSentGifts(BaseMember $member, Criteria $c)
+    {
+        $last_payment = $member->getLastCompletedPayment();
 
         if (!$last_payment) {
-            return false;
+            return 0;
         }
 
         $paymentDate = $last_payment->getCreatedAt(null); //timestamp
         $gracePeriod = sfConfig::get('app_gifts_send_period_days', 7)*24*60*60;  // 604800 secs
         $endDate = $paymentDate + $gracePeriod;
 
-        $c = new Criteria();
         $c->add(GiftPeer::FROM_MEMBER_ID, $member->getId());
         $c->add(GiftPeer::CREATED_AT, $paymentDate, Criteria::GREATER_THAN);
         $c->add(GiftPeer::CREATED_AT, $endDate, Criteria::LESS_THAN);
-        $c->add(GiftPeer::TO_EMAIL, $email);
-        $giftsCnt = GiftPeer::doCount($c);
 
-        return $giftsCnt > 0;
+        return GiftPeer::doCount($c);
     }
 }
