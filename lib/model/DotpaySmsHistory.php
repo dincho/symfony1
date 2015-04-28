6f3190406edab32e60e8f951f939c180fb78bca8
dincho <dincho@users.noreply.github.com>
2015-04-28 17:58:05 +0300

    add missing DotpaySmsHistory models (fixes #974)

---
+++ lib/model/DotpaySmsHistory.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'dotpay_sms_history' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class DotpaySmsHistory extends BaseDotpaySmsHistory
+{
+}
---
+++ lib/model/DotpaySmsHistoryPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'dotpay_sms_history' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class DotpaySmsHistoryPeer extends BaseDotpaySmsHistoryPeer
+{
+}
