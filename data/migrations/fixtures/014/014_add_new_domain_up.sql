4be488fbf4d3cc579464a7f3d27e0890d73fc1ba
dincho <dincho@users.noreply.github.com>
2015-04-30 19:16:24 +0300

    ashlay.pl is Polish domain

--- data/migrations/fixtures/014/014_add_new_domain_up.sql
+++ data/migrations/fixtures/014/014_add_new_domain_up.sql
@@ -6,7 +6,7 @@ VALUES
 INSERT INTO `trans_unit` (`id`,`cat_id`,`source`,`target`,`comments`,`author`,`translated`,`date_modified`,`msg_collection_id`,`date_added`,`tags`,`link`)
     SELECT NULL, 20, `source`,`target`,`comments`,`author`,`translated`,UNIX_TIMESTAMP(),`msg_collection_id`,UNIX_TIMESTAMP(),`tags`,`link` 
     FROM `trans_unit`
-    WHERE `cat_id` = 1;
+    WHERE `cat_id` = 2;
 
 INSERT INTO `subscription_details` (`id`, `subscription_id`, `cat_id`, `can_create_profile`, `create_profiles`, 
                  `can_post_photo`, `post_photos`, `can_post_private_photo`, `post_private_photos`, `can_wink`, 
@@ -19,27 +19,27 @@ INSERT INTO `subscription_details` (`id`, `subscription_id`, `cat_id`, `can_crea
                  `reply_messages`, `reply_messages_day`, `can_send_messages`, `send_messages`, `send_messages_day`, `can_see_viewed`, `see_viewed`,
                  `can_contact_assistant`, `contact_assistant`, `contact_assistant_day`, `private_dating`, `pre_approve`, `amount`, `currency`, `period`,`period_type`
     FROM `subscription_details`
-    WHERE `cat_id` = 1;
+    WHERE `cat_id` = 2;
 
 INSERT INTO `static_page_domain` (`link_name`, `title`, `keywords`, `description`, `content`, `id`, `updated_at`, `cat_id`)
     SELECT `link_name`, `title`, `keywords`, `description`, `content`, `id`, NOW(), 20
     FROM `static_page_domain`
-    WHERE `cat_id` = 1;
+    WHERE `cat_id` = 2;
 
 INSERT INTO `sf_setting` (`cat_id`, `env`, `name`, `value`, `description`, `created_user_id`, `updated_user_id`, `created_at`, `updated_at`, `var_type`)
     SELECT 20, `env`, `name`, `value`, `description`, `created_user_id`, `updated_user_id`, `created_at`, `updated_at`, `var_type`
     FROM `sf_setting`
-    WHERE `cat_id` = 1;
+    WHERE `cat_id` = 2;
 
 INSERT INTO `notification` (`send_from`, `send_to`, `reply_to`, `bcc`, `subject`, `body`, `footer`, `id`, `mail_config`, `cat_id`, `name`, `trigger_name`, `is_active`, `to_admins`, `days`, `whn`)
     SELECT `send_from`, `send_to`, `reply_to`, `bcc`, `subject`, `body`, `footer`, `id`, `mail_config`, 20, `name`, `trigger_name`, `is_active`, `to_admins`, `days`, `whn`
     FROM `notification`
-    WHERE `cat_id` = 1;
+    WHERE `cat_id` = 2;
 
 INSERT INTO `trans_unit` (`id`,`cat_id`,`source`,`target`,`comments`,`author`,`translated`,`date_modified`,`msg_collection_id`,`date_added`,`tags`,`link`)
     SELECT NULL, 21, `source`,`target`,`comments`,`author`,`translated`,UNIX_TIMESTAMP(),`msg_collection_id`,UNIX_TIMESTAMP(),`tags`,`link` 
     FROM `trans_unit`
-    WHERE `cat_id` = 1;
+    WHERE `cat_id` = 2;
 
 INSERT INTO `subscription_details` (`id`, `subscription_id`, `cat_id`, `can_create_profile`, `create_profiles`, 
                  `can_post_photo`, `post_photos`, `can_post_private_photo`, `post_private_photos`, `can_wink`, 
